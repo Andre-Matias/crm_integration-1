503257e1d60ff10c5d0d51f62e8b1152eed4758f
pedmatos <pedmigmatos@gmail.com>
2017-03-06 11:51:25 +0000

    Add files via upload

--- R/PM0001-StoriaStradiaDuplications/app.R
+++ R/PM0001-StoriaStradiaDuplications/app.R
@@ -1,48 +1,30 @@
 
+
 library(ggplot2)
-library(reshape2)
 library(shiny)
-library(DT)
-library(formattable)
-library(scales)
-library(eeptools)
+
 
 server <- function(input, output) {
   
   
   #load date 
   load("dfstoriadup.RData")
-  load("Storiadupfinal2.RData")
   load("dfstradia.RData")
-  load("Stradiadupfinal3.Rdata")
   
-  #Ajust similarity percentage (Shiny has some problems with that)
-  
-  Storiadupfinal2$similarity <- paste(round(Storiadupfinal2$similarity*100,digits=1),"%",sep="")
-  
-  Stradiadupfinal3$similarity <- paste(round(Stradiadupfinal3$similarity*100,digits=1),"%",sep="")
-  
-  #Tables(daily duplicates)
-  
-  output$ex1 <- DT::renderDataTable(
-    DT::datatable(Storiadupfinal2, options = list(pageLength = 25))
-  )
-  
-  output$ex2 <- DT::renderDataTable(
-    DT::datatable(Stradiadupfinal3, options = list(pageLength = 25))
-  )
-  
-  
+
   #plot (evolution by month)
   
   #Storia Graph 
+  
+  options(scipen=10000)
+  
   output$duplicatesPlot <- renderPlot({
     
     ggplot(df, aes(Date)) + 
       geom_bar(width=.5,aes(y = Ads, color = "Ads"), stat="identity", fill = "orange") +
       geom_line(aes(y = Duplicates, group = 1, color = "Duplicates")) +
       scale_colour_manual("", values=c("Duplicates" = "blue", "Ads" = "orange")) + 
-      coord_cartesian(ylim = c(70000, 320000)) + 
+      coord_cartesian(ylim = c(70000, 350000)) + 
       geom_text(aes(y= Duplicates,label = Duplicates, vjust=-2)) +
       geom_text(aes(y= Ads,label = Ads, vjust=2))
     
@@ -87,14 +69,8 @@ ui <- navbarPage(
              h6("Source: Database"),
              h6("Author: Pedro Matos"))),  
   tabPanel('Storia Graph', plotOutput("duplicatesPlot")),   
-  tabPanel('Storia Table', DT::dataTableOutput('ex1')),
   tabPanel('Stradia Graph', plotOutput("duplicatesPlot2")),   
-  tabPanel('Stradia Table', DT::dataTableOutput('ex2'))
-  
            )
-
-
-
 
 shinyApp(ui = ui, server = server)
 
