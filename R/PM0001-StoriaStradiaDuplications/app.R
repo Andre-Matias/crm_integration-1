dd198ed88790847225c412088fe9346235d8e91a
pedmigmatos <pedro.matos@olx.com>
2017-03-21 15:02:23 +0000

    Data Update
    New Duplicates Report

--- R/PM0001-StoriaStradiaDuplications/app.R
+++ R/PM0001-StoriaStradiaDuplications/app.R
@@ -9,16 +9,16 @@ server <- function(input, output) {
   
   #load date 
   load("dfstoriadup.RData")
-  load("Storiadupfinal2.RData")
+  load("Storiadupfinal.RData")
   load("dfstradia.RData")
-  load("Stradiadupfinal3.RData")
+  load("Stradiadupfinal.RData")
+  load("dfstoriadel1.RData")
+  load("dfstradiadel1.RData")
   
   
   #Ajust similarity percentage (Shiny has some problems with that)
   
   Storiadupfinal2$similarity <- paste(round(Storiadupfinal2$similarity*100,digits=1),"%",sep="")
-  
-  Stradiadupfinal3$similarity <- paste(round(Stradiadupfinal3$similarity*100,digits=1),"%",sep="")
   
   #Tables(daily duplicates)
   
@@ -44,13 +44,15 @@ server <- function(input, output) {
       geom_bar(width=.5,aes(y = Ads, color = "Ads"), stat="identity", fill = "orange") +
       geom_line(aes(y = Duplicates, group = 1, color = "Duplicates")) +
       scale_colour_manual("", values=c("Duplicates" = "blue", "Ads" = "orange")) + 
-      coord_cartesian(ylim = c(70000, 350000)) + 
+      coord_cartesian(ylim = c(30000, 350000)) + 
       geom_text(aes(y= Duplicates,label = Duplicates, vjust=-2)) +
-      geom_text(aes(y= Ads,label = Ads, vjust=2))
-    
+      geom_text(aes(y= Ads,label = Ads, vjust=2)) + 
+      ggtitle("Active Ads vs Active Duplicates by Day") +
+      theme(plot.title = element_text(lineheight=.8, face="bold"))
     
   })
   
+
   #Stradia Graph
   output$duplicatesPlot2 <- renderPlot({
     
@@ -60,27 +62,65 @@ server <- function(input, output) {
       scale_colour_manual("", values=c("Duplicates" = "blue", "Ads" = "orange")) + 
       coord_cartesian(ylim = c(0, 55000)) + 
       geom_text(aes(y= Duplicates,label = Duplicates, vjust=-1)) + 
-      geom_text(aes(y= Ads,label = Ads, vjust=2))
+      geom_text(aes(y= Ads,label = Ads, vjust=2)) + 
+      ggtitle("Active Ads vs Active Duplicates by Day") + 
+      theme(plot.title = element_text(lineheight=.8, face="bold")) 
     
   })
   
+  output$DeletedPlot <- renderPlot({
+    
+    ggplot(dfstoriadel1, aes(Date)) +
+      geom_line(aes(y = dfstoriadel1$"Deleted 7 Days %", group = 1, color = "Deleted 7 Days %")) +  
+      geom_line(aes(y = dfstoriadel1$"Deleted 3 Days %",group = 1, color = "Deleted 3 Days %")) +
+      geom_line(aes(y = dfstoriadel1$"Deleted 1 Day %", group = 1, color = "Deleted 1 Day %")) +
+      scale_colour_manual("", values=c("Deleted 3 Days %" = "blue","Deleted 7 Days %" = "orange","Deleted 1 Day %" = "brown")) +
+      geom_text(aes(y= dfstoriadel1$"Deleted 7 Days %",label = percent(dfstoriadel1$"Deleted 7 Days %"), vjust=1)) + 
+      geom_text(aes(y= dfstoriadel1$"Deleted 3 Days %",label = percent(dfstoriadel1$"Deleted 3 Days %"), vjust=1)) + 
+      geom_text(aes(y= dfstoriadel1$"Deleted 1 Day %",label = percent(dfstoriadel1$"Deleted 1 Day %"), vjust=1)) + 
+      ylab("Deleted Ads %") + scale_y_continuous(breaks = seq(0, 1, 0.01),labels=percent) + 
+      ggtitle("%Deleted ads based on creation date of the last 7, 3 and 1 Day") + 
+      theme(plot.title = element_text(lineheight=.8, face="bold")) 
+    
+  }) 
+  
+  output$DeletedPlot2 <- renderPlot({
+  
+  ggplot(dfstradiadel1, aes(Date)) +
+    geom_line(aes(y = dfstradiadel1$"Deleted 7 Days %", group = 1, color = "Deleted 7 Days %")) +  
+    geom_line(aes(y = dfstradiadel1$"Deleted 3 Days %",group = 1, color = "Deleted 3 Days %")) +
+    geom_line(aes(y = dfstradiadel1$"Deleted 1 Day %", group = 1, color = "Deleted 1 Day %")) +
+    scale_colour_manual("", values=c("Deleted 3 Days %" = "blue","Deleted 7 Days %" = "orange","Deleted 1 Day %" = "brown")) +
+    geom_text(aes(y= dfstradiadel1$"Deleted 7 Days %",label = percent(dfstradiadel1$"Deleted 7 Days %"), vjust=1)) + 
+    geom_text(aes(y= dfstradiadel1$"Deleted 3 Days %",label = percent(dfstradiadel1$"Deleted 3 Days %"), vjust=1)) + 
+    geom_text(aes(y= dfstradiadel1$"Deleted 1 Day %",label = percent(dfstradiadel1$"Deleted 1 Day %"), vjust=1)) + 
+    ylab("Deleted Ads %") + scale_y_continuous(breaks = seq(0, 1, 0.2),labels=percent) + 
+    ggtitle("%Deleted ads based on creation date of the last 7, 3 and 1 Day") + 
+    theme(plot.title = element_text(lineheight=.8, face="bold"))  
+  
+})
+  
 }
 
+  
 ui <- navbarPage(
   title = (""),
-  tabPanel('Overview',titlePanel("Storia.IND and Stradia.ID Duplicated Ads"),
+  tabPanel('Overview',titlePanel("Storia.IND and Stradia.IN Duplicated and Deleted Ads"),
            mainPanel(
-             h5("This report provides data about ad duplications for Storia Indonesia and Stradia India based on some variables."),
+             h5("This report provides data about ad duplications and deleted for Storia Indonesia and Stradia India based on some variables."),
              br(),
              h5("To find ad duplicates we use the following variables for Storia: 
                 Same user id, same city id, same category id, same price and title similarity with at least 70%.
                 For Stradia we use the following variables:
-                Same user id, same brand, same model, same year, same mileage and description similarity with at least 70%."),
+                Same user id, same brand, same model, same year, same mileage, same price and same fuel (rounded off)."),
              br(),
-             h5("In Storia and Stradia graph tabs, we have a plot with the relation between active ads and ad duplicates, by day. 
-                Due performance and memory capacity reasons, we just consider a maximum of 30 days per plot. With that we can have an overall perspective regarding the evolution of duplicates."), 
+             h5("In Storia and Stradia dup graph tabs we have a plot with the relation between active ads and ad duplicates, by day. 
+                Due performance and memory capacity reasons, we just consider a maximum of 30 days per plot. With this we can have an overall perspective regarding the evolution of duplicates."), 
              br(),
-             h5("In Storia and Stradia tables you can find the current duplications (daily active) and you can look deep for the data using the variables that we used."),
+             h5("On Storia and Stradia del graphs we can see the % of deleted ads considering the creation date (NNL) for the last 7, 3 and 1 day."),
+             br(),
+             h5("On Storia and Stradia tables you can find the current duplications (daily active) and you can look deep for the data using the variables that we used.
+                These differences between both platforms help us to understand why Stradia has such a low number of active duplicates."),
              br(),
              h5("We need to consider that some users can cheat us with different prices, titles, description, mileages or even year so is expected that we might have more duplicates than this report shows."),
              br(), 
@@ -88,10 +128,12 @@ ui <- navbarPage(
              h6("Date: 30 days for graphs, current day for tables"),
              h6("Source: Database"),
              h6("Author: Pedro Matos"))),  
-  tabPanel('Storia Graph', plotOutput("duplicatesPlot")),   
-  tabPanel('Storia Table', dataTableOutput('ex1')),
-  tabPanel('Stradia Graph', plotOutput("duplicatesPlot2")),   
-  tabPanel('Stradia Table', dataTableOutput('ex2'))
+  tabPanel('Storia Dup Graph', plotOutput("duplicatesPlot")),   
+  tabPanel('Storia Dup Table', dataTableOutput('ex1')),
+  tabPanel('Stradia Dup Graph', plotOutput("duplicatesPlot2")),   
+  tabPanel('Stradia Dup Table', dataTableOutput('ex2')),
+  tabPanel('Storia Del Graph',plotOutput("DeletedPlot")),
+  tabPanel('Stradia Del Graph',plotOutput("DeletedPlot2"))
   
 )
 
