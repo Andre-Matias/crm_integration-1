974a78ea2bc5644f128717749fbc1f4aa714a695
pedmatos <pedmigmatos@gmail.com>
2017-06-09 19:22:28 +0100

    Update data

--- R/PM0002-ImovirtualMigration/body/body_migration.R
+++ R/PM0002-ImovirtualMigration/body/body_migration.R
@@ -125,7 +125,7 @@ server_migration <- function(input, output, session) {
   
   # fluid row 1, kpi 3: Revenue
   output$revenueBox <- renderValueBox({
-    valueBox("8 040",
+    valueBox("6 981",
              "Revenue",
              icon = icon("money"),
              color = "olive")
