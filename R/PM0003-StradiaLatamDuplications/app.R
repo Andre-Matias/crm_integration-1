7504da2287c0bfbf6f85dccdce49a38f6e2638e3
pedmatos <pedmigmatos@gmail.com>
2017-07-05 15:27:09 +0100

    Update Data

--- R/PM0003-StradiaLatamDuplications/app.R
+++ R/PM0003-StradiaLatamDuplications/app.R
@@ -65,7 +65,7 @@ server <- function(input, output) {
       geom_bar(width=.8,aes(y = Ads, color = "Ads"), stat="identity", fill = "orange") +
       geom_line(aes(y = Duplicates, group = 1, color = "Duplicates")) +
       scale_colour_manual("", values=c("Duplicates" = "blue", "Ads" = "orange")) + 
-      coord_cartesian(ylim = c(0, 4500)) + 
+      coord_cartesian(ylim = c(0, 5200)) + 
       geom_text(aes(y= Duplicates,label =paste0(Duplicates,"\n",perduplicates), vjust=-1)) +
       geom_text(aes(y= Ads,label = Ads, vjust=2)) +
       scale_x_date(date_breaks="2 days", date_labels="%d%b") + 
