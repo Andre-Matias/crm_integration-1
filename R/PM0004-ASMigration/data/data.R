5d19baf321fd843031f25f140474d17a7b145b70
pedmatos <pedmigmatos@gmail.com>
2017-07-10 17:02:23 +0100

    Update Data

---
+++ R/PM0004-ASMigration/data/data.R
@@ -0,0 +1,7 @@
+load("UsersAstotal.RData")
+load("avgsessionatlas.RData")
+load("NNLatlas.RData")
+load("NNLhour.RData")
+
+
+
---
+++ R/PM0004-ASMigration/header/header.R
@@ -0,0 +1,13 @@
+# for the dashboard header, enter a title of "Global Sales Overview"
+ddm_notifications <-dropdownMenu(type = "notifications",
+                                 notificationItem(
+                                   text = paste("Data last updated on ", "2017-07-07", "13:24"),
+                                   icon("clock-o"),
+                                   status = "success"
+                                 )
+)
+
+# for the dashboard header, enter a title of "Global Sales Overview"
+header <- dashboardHeader(title="AS Migration KPI",ddm_notifications)
+
+
