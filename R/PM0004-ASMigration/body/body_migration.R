e97711dcad8df0c38cc17529ceadfda3889997c8
pedmatos <pedmigmatos@gmail.com>
2017-07-13 14:38:37 +0100

    Update Data

--- R/PM0004-ASMigration/body/body_migration.R
+++ R/PM0004-ASMigration/body/body_migration.R
@@ -79,14 +79,14 @@ server_migration <- function(input, output, session) {
  
  # fluid row 1, kpi 1: DAU
   output$dauBox <- renderValueBox({
-        valueBox("1 387", "DAU",
+        valueBox("1 417", "DAU",
            icon = icon("user"),
        color = "orange")
  })
   
   # fluid row 1, kpi 2: Avegare Session 
   output$AvgSeBox <- renderValueBox({
-    valueBox("24:04m", "Average Session",
+    valueBox("23:52m", "Average Session",
              icon = icon("clock-o"),
              color = "blue")
   })
@@ -94,7 +94,7 @@ server_migration <- function(input, output, session) {
 
   # fluid row 1, kpi 5: NNL
   output$nnlBox <- renderValueBox({
-    valueBox("5 035",
+    valueBox("1 827",
              "NNL",
              icon = icon("sign-in"),
              color = "maroon")
--- R/PM0004-ASMigration/header/header.R
+++ R/PM0004-ASMigration/header/header.R
@@ -1,7 +1,7 @@
 # for the dashboard header, enter a title of "Global Sales Overview"
 ddm_notifications <-dropdownMenu(type = "notifications",
                                  notificationItem(
-                                   text = paste("Data last updated on ", "2017-07-12", "19:28"),
+                                   text = paste("Data last updated on ", "2017-07-13", "14:37"),
                                    icon("clock-o"),
                                    status = "success"
                                  )
