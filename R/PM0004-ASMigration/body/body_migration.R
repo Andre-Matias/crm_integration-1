7c3f9e0b12061cc4e2876f4c1b2f449bce6f85b2
pedmatos <pedmigmatos@gmail.com>
2017-07-18 10:04:03 +0100

    Update Data

--- R/PM0004-ASMigration/body/body_migration.R
+++ R/PM0004-ASMigration/body/body_migration.R
@@ -79,14 +79,14 @@ server_migration <- function(input, output, session) {
  
  # fluid row 1, kpi 1: DAU
   output$dauBox <- renderValueBox({
-        valueBox("826", "DAU",
+        valueBox("1 576", "DAU",
            icon = icon("user"),
        color = "orange")
  })
   
   # fluid row 1, kpi 2: Avegare Session 
   output$AvgSeBox <- renderValueBox({
-    valueBox("22:51m", "Average Session",
+    valueBox("23:15m", "Average Session",
              icon = icon("clock-o"),
              color = "blue")
   })
@@ -94,7 +94,7 @@ server_migration <- function(input, output, session) {
 
   # fluid row 1, kpi 5: NNL
   output$nnlBox <- renderValueBox({
-    valueBox("1 208",
+    valueBox("3 806",
              "NNL",
              icon = icon("sign-in"),
              color = "maroon")
