57af57e1e0c489aa41dcba741028245a9c74dba0
pedmatos <pedmigmatos@gmail.com>
2017-07-28 15:15:45 +0100

    Update Data

---
+++ R/PM0005-Storia ID Migration/data/data.R
@@ -0,0 +1,9 @@
+load("UsersStorTotal.RData")
+load("ConversionRate.RData") 
+load("nnlstoria.RData")
+load("StoriaReplies.RData")
+load("UsersSource.RData")
+load("Activeads.RData")
+
+
+
---
+++ R/PM0005-Storia ID Migration/header/header.R
@@ -0,0 +1,13 @@
+# for the dashboard header, enter a title of "Global Sales Overview"
+ddm_notifications <-dropdownMenu(type = "notifications",
+                                 notificationItem(
+                                   text = paste("Data last updated on ", "2017-07-28", "10:18"),
+                                   icon("clock-o"),
+                                   status = "success"
+                                 )
+)
+
+# for the dashboard header, enter a title of "Global Sales Overview"
+header <- dashboardHeader(title="Storia ID Migration KPI",ddm_notifications)
+
+
