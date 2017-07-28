2d6c5d020a3c3b37f604aba54868ba77897f1ae2
pedmatos <pedmigmatos@gmail.com>
2017-07-28 09:13:50 +0100

    Update Data

--- R/PM0004-ASMigration/header/header.R
+++ R/PM0004-ASMigration/header/header.R
@@ -1,7 +1,7 @@
 # for the dashboard header, enter a title of "Global Sales Overview"
 ddm_notifications <-dropdownMenu(type = "notifications",
                                  notificationItem(
-                                   text = paste("Data last updated on ", "2017-07-27", "13:55"),
+                                   text = paste("Data last updated on ", "2017-07-27", "14:09"),
                                    icon("clock-o"),
                                    status = "success"
                                  )
