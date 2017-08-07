4395fc629a3050adf4fa5ba0f501730ab70b04b2
danielrochaolx <daniel.rocha@olx.com>
2017-08-07 12:20:29 +0100

    add auto save

--- scripts/r/9.Config/ConfigRdataGenerator.R
+++ scripts/r/9.Config/ConfigRdataGenerator.R
@@ -1,4 +1,10 @@
+# remove all object from workspace ---------------------------------------------------
+rm(list = ls())
+
 # Stradia India BI Slave -------------------------------------------------------------
 cfStradiaInDbHost <- "192.168.1.5"
 cfStradiaInDbPort <- "3312"
 cfStradiaInDbUser <- "bi_team_pt"
+
+# save all object in workspace -------------------------------------------------------
+save(list = ls(), file= "GlobalConfig.Rdata")
