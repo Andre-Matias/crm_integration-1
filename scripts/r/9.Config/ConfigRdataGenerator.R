a36778162bc5cdceba78806130d22f48c8f770b3
danielrochaolx <daniel.rocha@olx.com>
2017-08-07 12:17:21 +0100

    create file

---
+++ scripts/r/9.Config/ConfigRdataGenerator.R
@@ -0,0 +1,4 @@
+# Stradia India BI Slave -------------------------------------------------------------
+cfStradiaInDbHost <- "192.168.1.5"
+cfStradiaInDbPort <- "3312"
+cfStradiaInDbUser <- "bi_team_pt"
