5cc8dab2b845fff5d399dcf3853491ec3e89ddbe
MChinOLX <miguel.chin@olx.com>
2017-08-07 16:21:13 +0100

    Added additional resource extraction to Base
    Orders, Call and Call outcome

--- Pentaho ETL/Staging/Pythonadas/basecrm/Fulldump/fulldump.py
+++ Pentaho ETL/Staging/Pythonadas/basecrm/Fulldump/fulldump.py
@@ -22,13 +22,18 @@ client = basecrm.Client(access_token=access_token_base)
 s3_fulldump_deals(client,keyId,sKeyId,bucketName,path)
 s3_fulldump_contacts(client,keyId,sKeyId,bucketName,path)
 s3_fulldump_leads(client,keyId,sKeyId,bucketName,path)
-
 s3_fulldump_users(client,keyId,sKeyId,bucketName,path)
-s3_fulldump_stages(client,keyId,sKeyId,bucketName,path)
+s3_fulldump_tags(client,keyId,sKeyId,bucketName,path)
+#Missing Line items, orders an products
 
+
+These are not available in Firehose
+s3_fulldump_stages(client,keyId,sKeyId,bucketName,path)
 s3_fulldump_loss_reasons(client,keyId,sKeyId,bucketName,path)
 s3_fulldump_notes(client,keyId,sKeyId,bucketName,path)
-
 s3_fulldump_pipelines(client,keyId,sKeyId,bucketName,path)
 s3_fulldump_sources(client,keyId,sKeyId,bucketName,path)
-s3_fulldump_tags(client,keyId,sKeyId,bucketName,path)
+
+s3_fulldump_orders(access_token_base,keyId,sKeyId,bucketName,path)
+s3_fulldump_calls(access_token_base,keyId,sKeyId,bucketName,path)
+s3_fulldump_calls_outcome(access_token_base,keyId,sKeyId,bucketName,path)
