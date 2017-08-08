ae834fa9d26b33ca88dbefa41fd32c5579642cbf
MChinOLX <miguel.chin@olx.com>
2017-08-08 13:00:01 +0100

    Disabled order requests
    Disabled order requests

--- Pentaho ETL/Staging/Pythonadas/basecrm/Fulldump/fulldump.py
+++ Pentaho ETL/Staging/Pythonadas/basecrm/Fulldump/fulldump.py
@@ -24,7 +24,7 @@ s3_fulldump_contacts(client,keyId,sKeyId,bucketName,path)
 s3_fulldump_leads(client,keyId,sKeyId,bucketName,path)
 s3_fulldump_users(client,keyId,sKeyId,bucketName,path)
 s3_fulldump_tags(client,keyId,sKeyId,bucketName,path)
-s3_fulldump_orders(access_token_base,keyId,sKeyId,bucketName,path)
+#s3_fulldump_orders(access_token_base,keyId,sKeyId,bucketName,path)
 s3_fulldump_calls(access_token_base,keyId,sKeyId,bucketName,path)
 
 
