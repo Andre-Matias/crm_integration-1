00197ff24d0377c327925789bac4a62aeb6f5c32
MChinOLX <miguel.chin@olx.com>
2017-08-09 17:59:47 +0100

    Added missing schema
    Added missing schema

--- Pentaho ETL/Staging/Pythonadas/basecrm/Fulldump/load_resources.py
+++ Pentaho ETL/Staging/Pythonadas/basecrm/Fulldump/load_resources.py
@@ -79,11 +79,12 @@ def truncateResourceTables(conf_file,schema,resources,platform,prefix):
 	cur = conn.cursor()
 
 	for resource in resources:
-		cur.execute("TRUNCATE TABLE %(prefix)sstg_d_base_%(resource)s_%(platform)s") 
+		cur.execute("TRUNCATE TABLE %(schema)s.%(prefix)sstg_d_base_%(resource)s_%(platform)s") 
 			% {
 			'resource':resource,
 			'platform':platform,
-			'prefix': prefix
+			'prefix': prefix,
+			'schema': schema
 			}
 
 	conn.commit()
