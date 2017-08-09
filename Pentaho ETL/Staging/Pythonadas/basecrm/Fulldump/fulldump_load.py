382bcea87a5313c538632a201c7a30120ff7e23a
MChinOLX <miguel.chin@olx.com>
2017-08-09 17:22:45 +0100

    Added fulldump loads on redshift
    Added fulldump loads on redshift

---
+++ Pentaho ETL/Staging/Pythonadas/basecrm/Fulldump/fulldump_load.py
@@ -0,0 +1,25 @@
+from load_resources import *
+
+conf_file = sys.argv[1]
+chandra_conf_file = sys.argv[2]
+
+file = open(conf_file, "r") 
+temp = file.read().splitlines()
+bucketName = temp[7]
+path_fulldump = temp[9]
+manifest = temp[11]
+schema = temp[13]
+platform = temp[15]
+resources = temp[17].split(',')
+file.close()
+
+
+# prefix parameter should be 'sync_' or ''
+loadFromS3toRedshift(chandra_conf_file, 
+	schema,
+	platform,
+	bucketName,
+	path_fulldump,
+	manifest,
+	resources,
+	'')
