414ba1f2c9f24f8b02580a390cd4828e9d658d0f
danielrochaolx <daniel.rocha@olx.com>
2017-08-11 09:48:08 +0000

    make script generic

--- scripts/r/8.GlobalScripts/MixpanelGetJQLData.R
+++ scripts/r/8.GlobalScripts/MixpanelGetJQLData.R
@@ -7,16 +7,8 @@ library("RMixpanel")
 # load mixpanel user's credentials --------------------------------------------
 load("~/credentials.Rdata")
 
+# load JQL 
 source(file = "../../jql/NewUsersCohortTimeToFirstReply.jql")
-
-jqlQuery <- '
-function main() {
-  return Events({
-    from_date: "2017-08-01",
-    to_date: "2017-08-01"
-  })
-  .groupByUser(mixpanel.reducer.count())
-}'
 
 result <- mixpanelJQLQuery(mixpanelOtomotoAccount, jqlQuery)
 
