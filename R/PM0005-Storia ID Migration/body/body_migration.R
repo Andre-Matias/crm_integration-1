d5549a77f2bd90c647e55bd9f4269e27af97d22e
mcpasin0lx <marco.pasin@olx.com>
2017-08-21 09:40:18 +0100

    Update 21/08/2017

--- R/PM0005-Storia ID Migration/body/body_migration.R
+++ R/PM0005-Storia ID Migration/body/body_migration.R
@@ -89,14 +89,14 @@ server_migration <- function(input, output, session) {
  
  # fluid row 1, kpi 1: DAU
   output$dauBox <- renderValueBox({
-        valueBox("715", "DAU",
+        valueBox("820", "DAU",
            icon = icon("user"),
        color = "orange")
  })
   
   # fluid row 1, kpi 2: DAU by Source
   output$sourceBox <- renderValueBox({
-    valueBox("715","DAU by Source",
+    valueBox("820","DAU by Source",
              icon = icon("users"),
              color = "blue")
   })
@@ -104,7 +104,7 @@ server_migration <- function(input, output, session) {
   
   # fluid row 1, kpi 3: Replies
   output$repliesBox <- renderValueBox({
-    valueBox("0", "Message Replies",
+    valueBox("3", "Message Replies",
              icon = icon("reply"),
              color = "yellow")
   })
@@ -112,7 +112,7 @@ server_migration <- function(input, output, session) {
 
   # fluid row 1, kpi 4: NNL
   output$nnlBox <- renderValueBox({
-    valueBox("1 294 ",
+    valueBox("1 061 ",
              "NNL",
              icon = icon("sign-in"),
              color = "maroon")
@@ -120,7 +120,7 @@ server_migration <- function(input, output, session) {
  
   #  fluid row 1, kpi 5: Active Ads 
  output$activeadsBox <- renderValueBox({
- valueBox("625 287",
+ valueBox("606 903",
         "Active Ads",
          icon = icon("building"),
         color = "teal")
--- R/PM0005-Storia ID Migration/header/header.R
+++ R/PM0005-Storia ID Migration/header/header.R
@@ -1,7 +1,7 @@
 # for the dashboard header, enter a title of "Global Sales Overview"
 ddm_notifications <-dropdownMenu(type = "notifications",
                                  notificationItem(
-                                   text = paste("Data last updated on ", "2017-08-18", "11:55"),
+                                   text = paste("Data last updated on ", "2017-08-21", "09:35"),
                                    icon("clock-o"),
                                    status = "success"
                                  )
