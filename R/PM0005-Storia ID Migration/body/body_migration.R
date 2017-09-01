20ebb49034b030cb820019e74f7688c483ba135f
mcpasin0lx <marco.pasin@olx.com>
2017-09-01 10:56:12 +0100

    Update data

--- R/PM0005-Storia ID Migration/body/body_migration.R
+++ R/PM0005-Storia ID Migration/body/body_migration.R
@@ -89,14 +89,14 @@ server_migration <- function(input, output, session) {
  
  # fluid row 1, kpi 1: DAU
   output$dauBox <- renderValueBox({
-        valueBox("734", "DAU",
+        valueBox("395", "DAU",
            icon = icon("user"),
        color = "orange")
  })
   
   # fluid row 1, kpi 2: DAU by Source
   output$sourceBox <- renderValueBox({
-    valueBox("734","DAU by Source",
+    valueBox("395","DAU by Source",
              icon = icon("users"),
              color = "blue")
   })
@@ -112,7 +112,7 @@ server_migration <- function(input, output, session) {
 
   # fluid row 1, kpi 4: NNL
   output$nnlBox <- renderValueBox({
-    valueBox("3 092 ",
+    valueBox("2 769 ",
              "NNL",
              icon = icon("sign-in"),
              color = "maroon")
@@ -120,7 +120,7 @@ server_migration <- function(input, output, session) {
  
   #  fluid row 1, kpi 5: Active Ads 
  output$activeadsBox <- renderValueBox({
- valueBox("620 491",
+ valueBox("620 996",
         "Active Ads",
          icon = icon("building"),
         color = "teal")
@@ -128,7 +128,7 @@ server_migration <- function(input, output, session) {
 
   # fluid row 1, kpi 6: Conversion Rate
   output$conversionBox <- renderValueBox({
-    valueBox("1.1%","Reply Conversion","(by Ad visits)",
+    valueBox("1.9%","Reply Conversion","(by Ad visits)",
              icon = icon("check-circle"),
              color = "olive")
   })
--- R/PM0005-Storia ID Migration/header/header.R
+++ R/PM0005-Storia ID Migration/header/header.R
@@ -1,7 +1,7 @@
 # for the dashboard header, enter a title of "Global Sales Overview"
 ddm_notifications <-dropdownMenu(type = "notifications",
                                  notificationItem(
-                                   text = paste("Data last updated on ", "2017-08-31", "09:20"),
+                                   text = paste("Data last updated on ", "2017-09-01", "10:30"),
                                    icon("clock-o"),
                                    status = "success"
                                  )
