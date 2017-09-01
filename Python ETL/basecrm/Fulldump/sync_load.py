33bcca079b3ddb3204bbfead17bda149636f7acb
MChinOLX <miguel.chin@olx.com>
2017-09-01 15:27:54 +0100

    Changed conf file
    Changed conf file

--- Python ETL/basecrm/Fulldump/sync_load.py
+++ Python ETL/basecrm/Fulldump/sync_load.py
@@ -61,7 +61,7 @@ syncContactsTable(target_conf_file,schema,category,country)
 syncLeadsTable(target_conf_file,schema,category,country)
 syncUsersTable(target_conf_file,schema,category,country)
 syncCallsTable(target_conf_file,schema,category,country)
-syncTagsTable(conf_file,schema,category,country)
+syncTagsTable(target_conf_file,schema,category,country)
 #Line Items, Orders
 
 
