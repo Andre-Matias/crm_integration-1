c5239b4c39e56b7ab8eab1abcbf6166cb9b3c85c
MChinOLX <miguel.chin@olx.com>
2017-09-01 15:31:25 +0100

    Removed drop view
    Removed drop view

--- Python ETL/basecrm/Fulldump/load_resources.py
+++ Python ETL/basecrm/Fulldump/load_resources.py
@@ -538,7 +538,6 @@ def syncTagsTable(conf_file,schema,category,country):
 			"); "\
 			"INSERT INTO %(schema)s.stg_d_base_tags_debug "\
 			"(SELECT * FROM %(schema)s.sync_stg_d_base_tags_%(category)s_%(country)s_view); "\
-			"DROP VIEW %(schema)s.sync_stg_d_base_tags_%(category)s_%(country)s_view; "\
 			"DELETE FROM %(schema)s.stg_d_base_tags WHERE base_account_country = '%(country)s' AND base_account_category = '%(category)s' "\
 			"AND id IN (SELECT id FROM %(schema)s.sync_stg_d_base_tags_%(category)s_%(country)s_view); "\
 			"INSERT INTO %(schema)s.stg_d_base_tags "\
