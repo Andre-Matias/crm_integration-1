8eb8b5a5b5507fbe16ced0595059d570f715aca9
MChinOLX <miguel.chin@olx.com>
2017-09-07 17:14:02 +0100

    Removed try for debugging
    Removed try for debugging

--- Python ETL/basecrm/Fulldump/load_resources.py
+++ Python ETL/basecrm/Fulldump/load_resources.py
@@ -69,36 +69,33 @@ def loadFromS3toRedshift(conf_file,schema,category,country,bucket,data_path,date
 	if prefix == 'sync_':
 		for resource in resources:
 			print(resource)
-			try:
-				cur.execute(
-					getCopySql(
-						schema, \
-						'%(prefix)sstg_d_base_%(resource)s_%(category)s_%(country)s' \
-							% {
-							'resource':resource,
-							'category':category,
-							'country':country,
-							 'prefix': prefix},
-						's3://%(bucket)s%(data_path)s%(resource)s/%(date)s/' \
-							% {
-							'resource':resource,
-							'bucket':bucket,
-							'date': date,
-							'data_path':data_path},
-						's3://%(bucket)s%(manifest_path)s%(prefix)s%(resource)s_jsonpath.json' \
-							% {
-							'prefix': prefix,
-							'resource':resource,
-							'bucket':bucket,
-							'manifest_path':manifest_path
-							}, 
-						credentials
-					)
+
+			cur.execute(
+				getCopySql(
+					schema, \
+					'%(prefix)sstg_d_base_%(resource)s_%(category)s_%(country)s' \
+						% {
+						'resource':resource,
+						'category':category,
+						'country':country,
+						 'prefix': prefix},
+					's3://%(bucket)s%(data_path)s%(resource)s/%(date)s/' \
+						% {
+						'resource':resource,
+						'bucket':bucket,
+						'date': date,
+						'data_path':data_path},
+					's3://%(bucket)s%(manifest_path)s%(prefix)s%(resource)s_jsonpath.json' \
+						% {
+						'prefix': prefix,
+						'resource':resource,
+						'bucket':bucket,
+						'manifest_path':manifest_path
+						}, 
+					credentials
 				)
-			except(psycopg2.InternalError):
-				print("No file found")
-				pass
-			else: print("Something when wrong")	
+			)
+
 	conn.commit()
 
 	#Close connection
