aad6c5950a3b9c3b4d7e1fa12ddf3f1f4e9b9aee
MChinOLX <miguel.chin@olx.com>
2017-09-07 17:39:20 +0100

    Rolling back
    Rolling back

--- Python ETL/basecrm/Fulldump/load_resources.py
+++ Python ETL/basecrm/Fulldump/load_resources.py
@@ -69,35 +69,35 @@ def loadFromS3toRedshift(conf_file,schema,category,country,bucket,data_path,date
 	if prefix == 'sync_':
 		for resource in resources:
 			print(resource)
-
-			cur.execute("SELECT count(*) FROM rdl_basecrm_v2.stg_d_base_deals;")
-			print(np.array(cur.fetchall()))
-			cur.execute(
-				getCopySql(
-					schema, \
-					'%(prefix)sstg_d_base_%(resource)s_%(category)s_%(country)s' \
-						% {
-						'resource':resource,
-						'category':category,
-						'country':country,
-						 'prefix': prefix},
-					's3://%(bucket)s%(data_path)s%(resource)s/%(date)s/' \
-						% {
-						'resource':resource,
-						'bucket':bucket,
-						'date': date,
-						'data_path':data_path},
-					's3://%(bucket)s%(manifest_path)s%(prefix)s%(resource)s_jsonpath.json' \
-						% {
-						'prefix': prefix,
-						'resource':resource,
-						'bucket':bucket,
-						'manifest_path':manifest_path
-						}, 
-					credentials
+			try:
+				cur.execute(
+					getCopySql(
+						schema, \
+						'%(prefix)sstg_d_base_%(resource)s_%(category)s_%(country)s' \
+							% {
+							'resource':resource,
+							'category':category,
+							'country':country,
+							 'prefix': prefix},
+						's3://%(bucket)s%(data_path)s%(resource)s/%(date)s/' \
+							% {
+							'resource':resource,
+							'bucket':bucket,
+							'date': date,
+							'data_path':data_path},
+						's3://%(bucket)s%(manifest_path)s%(prefix)s%(resource)s_jsonpath.json' \
+							% {
+							'prefix': prefix,
+							'resource':resource,
+							'bucket':bucket,
+							'manifest_path':manifest_path
+							}, 
+						credentials
+					)
 				)
-			)
-
+			except(psycopg2.InternalError):
+				print("No file found")
+				pass
 	conn.commit()
 
 	#Close connection
