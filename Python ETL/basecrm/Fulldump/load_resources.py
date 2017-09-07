f5bd71b441c0dc3b4cf6e86983028ffae8af0489
MChinOLX <miguel.chin@olx.com>
2017-09-07 16:22:26 +0100

    Bug fixing
    Bug fixing

--- Python ETL/basecrm/Fulldump/load_resources.py
+++ Python ETL/basecrm/Fulldump/load_resources.py
@@ -64,7 +64,7 @@ def loadFromS3toRedshift(conf_file,schema,category,country,bucket,data_path,date
 					)
 				)
 			except(psycopg2.InternalError):
-				print("Internal Error, probably non existing file")
+				print("No file found")
 				pass
 	if prefix == 'sync_':
 		for resource in resources:
@@ -96,7 +96,7 @@ def loadFromS3toRedshift(conf_file,schema,category,country,bucket,data_path,date
 					)
 				)
 			except(psycopg2.InternalError):
-				print("Internal Error, probably non existing file")
+				print("No file found")
 				pass
 	conn.commit()
 
