eb5de957ae5aa0a8b1fe882b31311c4c7f7058f7
MChinOLX <miguel.chin@olx.com>
2017-09-08 13:56:37 +0100

    Debugging
    Debugging

--- Python ETL/basecrm/Fulldump/load_resources.py
+++ Python ETL/basecrm/Fulldump/load_resources.py
@@ -724,7 +724,6 @@ def deletePreviousS3Files(conf_file):
 		x.delete()
 
 def checkS3FileExists(conf_file,bucket,path):
-	print(path)
 	conf = json.load(open(conf_file))
 	key = conf['s3_key']
 	skey = conf['s3_skey']
@@ -734,6 +733,7 @@ def checkS3FileExists(conf_file,bucket,path):
 
 	for x in b.list(prefix = path):
 		if(len(str(x)) > 0):
+			print(path)
 			found_file = 'true'
 			break
 
