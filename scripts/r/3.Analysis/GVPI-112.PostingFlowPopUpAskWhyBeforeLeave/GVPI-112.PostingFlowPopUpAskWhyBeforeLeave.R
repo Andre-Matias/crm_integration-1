6865fb3421d5e558a663523b01411c36fae6d10e
danielrochaolx <daniel.rocha@olx.com>
2017-09-19 14:22:40 +0000

    upload df to S3

--- scripts/r/3.Analysis/GVPI-112.PostingFlowPopUpAskWhyBeforeLeave/GVPI-112.PostingFlowPopUpAskWhyBeforeLeave.R
+++ scripts/r/3.Analysis/GVPI-112.PostingFlowPopUpAskWhyBeforeLeave/GVPI-112.PostingFlowPopUpAskWhyBeforeLeave.R
@@ -1,9 +1,17 @@
 # load libraries --------------------------------------------------------------
 library("RMixpanel")
+library("data.table")
+library("dplyr")
+library("dtplyr")
+library("magrittr")
+library("aws.s3")
 
-# load mixpanel user's credentials --------------------------------------------
+# load credentials file -------------------------------------------------------
 load("~/credentials.Rdata")
+load("~/GlobalConfig.Rdata")
 
+Sys.setenv("AWS_ACCESS_KEY_ID" = myS3key,
+           "AWS_SECRET_ACCESS_KEY" = MyS3SecretAccessKey)
 # load JQL 
 source(file = "~/verticals-bi/scripts/jql/PostingFlowLeavingReason.jql")
 
@@ -24,9 +32,26 @@ dfStandvirtual$project <- "Standvirtual.PT"
 
 dfAll <- do.call(rbind, list(dfOtomoto, dfAutovit, dfStandvirtual))
 
+# define columns names --------------------------------------------------------
+colnames(dfAll) <- 
+  c("date", "event", "reason", "user_status", "platform", "business_status",
+    "value", "project")
 
+# clean -----------------------------------------------------------------------
 
+dfAll[dfAll$event == "posting_leaving_reason_show", c("reason")] <- NA
 
+# summarise -------------------------------------------------------------------
+dfAll <-
+  dfAll %>%
+  mutate(date = as.Date(date)) %>%
+  group_by(date, event, reason, user_status, platform,
+           business_status, project) %>%
+  summarise(qty = sum(as.numeric(value)))
 
+# save it to amazon s3 --------------------------------------------------------
 
+s3saveRDS(x = dfAll,
+          object = "PostingFlowDropReasons.RDS",
+          bucket = "pyrates-data-ocean/GVPI-112")
 
