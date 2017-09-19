3394641d14a9687659908cbc1ff60a0de361b28a
danielrochaolx <daniel.rocha@olx.com>
2017-09-19 14:23:18 +0000

    adding drop reasons graph

--- R/DR0001-AdventureGalley/app.R
+++ R/DR0001-AdventureGalley/app.R
@@ -1,5 +1,25 @@
-library(shiny)
-library(shinydashboard)
+# load libraries --------------------------------------------------------------
+library("shiny")
+library("shinydashboard")
+library("aws.s3")
+library("ggplot2")
+library("magrittr")
+library("data.table")
+library("dplyr")
+library("dtplyr")
+
+
+# load mixpanel user's credentials --------------------------------------------
+load("~/GlobalConfig.Rdata")
+load("~/credentials.Rdata")
+
+Sys.setenv("AWS_ACCESS_KEY_ID" = myS3key,
+           "AWS_SECRET_ACCESS_KEY" = MyS3SecretAccessKey)
+
+dfAll <- 
+  s3readRDS(object = "PostingFlowDropReasons.RDS", 
+            bucket = "pyrates-data-ocean/GVPI-112"
+  )
 
 ui <- 
   dashboardPage(
@@ -10,8 +30,10 @@ ui <-
   dashboardSidebar(
       sidebarMenu(
         menuItem("Monetization", tabName = 'tabMonetization', icon = icon('money'),
-                   menuItem('Posting Flow', tabName = 'tabPostingFlow', 
-                            menuSubItem('Drop Reasons', tabName = 'tabDropReasons'))
+                 menuItem('Posting Flow', tabName = 'tabPostingFlow', 
+                          menuSubItem('Drop Reasons', tabName = 'tabDropReasons')),
+                 menuItem('teste2', tabName = 'tabTeste2', 
+                          menuSubItem('', tabName = 'tab'))
                  )
       ),
       sidebarMenu(
@@ -21,10 +43,31 @@ ui <-
 # dashboardSidebar end --------------------------------------------------------
 
 # dashboardbody start ---------------------------------------------------------
-  dashboardBody()
+  dashboardBody(
+    tabItems(
+      # Monetization - Posting Flow - Drop Reasons 
+      tabItem(tabName = "tabDropReasons",
+              fluidRow(
+                box(plotOutput("d", height = 250))
+              )
+              )
+    )
+  )
 # dashboardbody end -----------------------------------------------------------
 )
-server <- function(input, output) { }
+server <- function(input, output) { 
+  
+  output$d <- renderPlot({
+    dfAll %>%
+    filter(project == "Otomoto.PL", event == "posting_leaving_reason") %>%
+    group_by(date, reason) %>%
+    summarise(qty = sum(qty)) %>%
+    mutate(perQty = qty / sum(qty)) %>%
+    ggplot() +
+    geom_line(stat = "identity", aes(date,perQty, group = reason, colour= reason))
+  })
+  
+  }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
