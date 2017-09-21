201ae525f1d991c728ab3e609b404885e6fb1460
mcpasin0lx <marco.pasin@olx.com>
2017-09-21 16:08:08 +0100

    adding 2nd okr (only for otomoto pl)

--- R/MP0001-OKRBaselineOnboarding/app.R
+++ R/MP0001-OKRBaselineOnboarding/app.R
@@ -3,9 +3,12 @@
 # the 'Run App' button above.
 
 library(shiny)
+library(dplyr)
 
 # Global
 source("ret_lead_cons_function.R")
+source("ret_anything.R")
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -23,11 +26,18 @@ ui <- fluidPage(
       
       # Show a plot of the generated distribution
       mainPanel(
-         # plotOutput("distPlot"),
-         plotOutput("retPlot", height = 600),
+         ## 1st okr
+         plotOutput("retPlot", height = 500),
          br(),
-         #tableOutput("retTable")
-         DT::dataTableOutput("table")
+         DT::dataTableOutput("retTable"),
+         
+         br(),
+         br(),
+         ## 2nd okr
+         plotOutput("retAnyPlot", height = 500),
+         br(),
+         DT::dataTableOutput("retAnyTable")
+         
       )
    )
 )
@@ -36,35 +46,43 @@ ui <- fluidPage(
 # Define server logic 
 server <- function(input, output) {
     
-    df <- reactive({
-      df<- filter(dNewUsers2, week %in% input$show_vars)
-      df
-    })
-  
-  
-   
+    # df <- reactive({
+    #   df<- filter(dNewUsers2, week %in% input$show_vars)
+    #   df
+    # })
+    
+
    output$retPlot <- renderPlot({
-      ggplot(data = df()) + geom_line(aes(x = TimeToConvert, y = ret, colour = week))+
+     dNewUsers2<- filter(dNewUsers2, week %in% input$show_vars)
+      ggplot(data = dNewUsers2) + geom_line(aes(x = TimeToConvert, y = ret, colour = week))+
        scale_y_continuous(labels = scales::percent, breaks = seq(0,0.20,0.01), limits = c(0,0.20))+
        scale_x_continuous(breaks = seq(0,30,1), limits = c(0,30))+ggtitle("New Users That Send a Lead - Time to Send (days)", subtitle = "Consolidated: otomoto.pl + standvirtual.pt + autovit.ro")+
        theme_fivethirtyeight()+theme(text = element_text(family = "Andale Mono"))+xlab("days to convert") + ylab("% new users") 
     
    })
    
-   # add data table
-   # output$retTable <- renderTable({
-   # ret_table_cons <- filter(ret_table_cons, week %in% input$show_vars)
-   # ret_table_cons  
-   # })
    
-   
-   output$table <- DT::renderDataTable({
+   output$retTable <- DT::renderDataTable({
      
      ret_table_cons <- filter(ret_table_cons, week %in% input$show_vars)
      DT::datatable(ret_table_cons, options = list(dom = 't'), class = "compact", rownames= FALSE)
    })
    
    # add 1st okr graph + data table
+   output$retAnyPlot <- renderPlot({
+     ret_any_2 <- filter(ret_any_2, week %in% input$show_vars)
+     ggplot(data = ret_any_2) + geom_line(aes(x = TimeToConvert, y = ret, colour = week))+
+       scale_y_continuous(labels = scales::percent, breaks = seq(0,0.13,0.01), limits = c(0,0.13))+
+       scale_x_continuous(breaks = seq(0,30,1), limits = c(0,30)) +ggtitle("New Users That Return and do Anything - Time to Return (days)", subtitle = " otomoto.pl")+
+       theme_fivethirtyeight()+theme(text = element_text(family = "Andale Mono")) + xlab("days to convert") + ylab("% new users") 
+     
+   })
+   
+   output$retAnyTable <- DT::renderDataTable({
+     
+     ret_any_table <- filter(ret_any_table, week %in% input$show_vars)
+     DT::datatable(ret_any_table, options = list(dom = 't'), class = "compact", rownames= FALSE)
+   })
    
 }
 
