56124abd08b6bd771c2b94dec0a6c21674d170f0
danielrochaolx <daniel.rocha@olx.com>
2017-09-21 17:22:28 +0000

    initial script

---
+++ R/DR0001-AdventureGalley/Module_GVPI-116.PostingFlowHideNotMandatoryFields.R
@@ -0,0 +1,80 @@
+# load libraries --------------------------------------------------------------
+library("shiny")
+library("shinydashboard")
+library("aws.s3")
+library("ggplot2")
+library("magrittr")
+library("data.table")
+library("dplyr")
+library("dtplyr")
+library("scales")
+library("ggthemes")
+library("ggplot2")
+
+
+# load credentials ------------------------------------------------------------
+load("~/GlobalConfig.Rdata")
+load("~/credentials.Rdata")
+
+Sys.setenv("AWS_ACCESS_KEY_ID" = myS3key,
+           "AWS_SECRET_ACCESS_KEY" = MyS3SecretAccessKey)
+
+rawDf <-
+  s3readRDS(object = "DescriptionState.RDS",
+            bucket = "pyrates-data-ocean/GVPI-116"
+  )
+
+# begin of UI module ----------------------------------------------------------
+module_GVPI116_UI <- function(id){
+  
+  ns <- NS(id)
+  
+  
+tagList(
+  
+    plotOutput(ns("HideDescriptionPlot")),
+    
+    checkboxGroupInput(
+      inputId = ns("gvpi116SelectPlatform"),
+      label = "Platform",
+      choices = unique(rawDf$platform)
+      ),
+    
+    checkboxGroupInput(
+      inputId = ns("gvpi116SelectCategories"),
+      label = "Category",
+      choices = unique(rawDf$name_en)
+      )
+  )
+# end of UI module ------------------------------------------------------------
+}
+
+# begin of SERVER module ------------------------------------------------------
+module_GVPI116 <- function(input, output, session){
+
+df <-
+  reactive({
+
+  tmp <-
+  rawDf%>%
+  filter(platform %in% input$gvpi116SelectPlatform
+         & name_en %in% input$gvpi116SelectCategories ) %>%
+  group_by(day, DescriptionState) %>%
+  summarise(qtyListings = sum(qtyListings)) %>%
+  mutate(
+    perListings = qtyListings / sum(qtyListings)
+  )
+  tmp
+  })
+
+
+output$HideDescriptionPlot <-
+  renderPlot({
+  ggplot(df())+
+  geom_line(
+    aes(x = day, y = perListings,
+        group = DescriptionState, color = DescriptionState))
+  })
+
+# end of SERVER module --------------------------------------------------------
+}
