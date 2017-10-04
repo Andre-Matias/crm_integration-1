d9bd0ba568b349bbd129744364441c6d8e125e39
MChinOLX <miguel.chin@olx.com>
2017-10-04 13:56:03 +0100

    Fixed indentation
    Fixed indentation

--- Python ETL/basecrm/Firehose/firehose_process_V1.py
+++ Python ETL/basecrm/Firehose/firehose_process_V1.py
@@ -35,23 +35,23 @@ bucketName=d['bucket_name']
 print(bucketName)
 
 def method_convert_custom_fields(custom_field_values):
-    convert_json = {}
-    print custom_field_values
-    for custom_fld in custom_field_values:
-        name = (custom_fld['custom_field']['data']['name'])
-        if (isinstance(custom_fld['value'],list)):
-            aux_array = []
-            for list_element in custom_fld['value']:
-                aux_array.append(list_element['name'])
-            convert_json[name] = aux_array  
-        if (isinstance(custom_fld['value'],dict)):
-        	if(custom_fld['value']['name'] is None):
-        		convert_json[name] = ""
-            else:
-            	convert_json[name] = custom_fld['value']['name']
-        else:
-            convert_json[name] = custom_fld['value']
-    return convert_json
+	convert_json = {}
+	print custom_field_values
+	for custom_fld in custom_field_values:
+		name = (custom_fld['custom_field']['data']['name'])
+		if (isinstance(custom_fld['value'],list)):
+			aux_array = []
+			for list_element in custom_fld['value']:
+				aux_array.append(list_element['name'])
+			convert_json[name] = aux_array  
+		if (isinstance(custom_fld['value'],dict)):
+			if(custom_fld['value']['name'] is None):
+				convert_json[name] = ""
+			else:
+				convert_json[name] = custom_fld['value']['name']
+		else:
+			convert_json[name] = custom_fld['value']
+	return convert_json
 	
 def method_convert_tags(tags):
     convert_tag = []
