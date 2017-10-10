70dbd7358165bf3f91d0eb325178ab1c41f19df2
pedmatos <pedmigmatos@gmail.com>
2017-10-10 16:00:47 +0100

    Update Data

--- R/PM0003-StradiaLatamDuplications/app.R
+++ R/PM0003-StradiaLatamDuplications/app.R
@@ -13,11 +13,18 @@ server <- function(input, output) {
   load("Stradiacodupfinal.RData")
   load("Stradiapedupfinal.RData")
   load("Stradiaecdupfinal.RData")
+  load("Otodomuadupfinal.RData")
   load("dfstradiar.RData")
   load("dfstradiaco.RData")
   load("dfstradiape.RData")
   load("dfstradiaec.RData")
+  load("dfotodomua.RData")
   
+  
+  
+  #Ajust similarity percentage (Shiny has some problems with that)
+  
+  Otodomuadupfinal2$similarity <- paste(round(Otodomuadupfinal2$similarity*100,digits=1),"%",sep="")
   
   #Tables(daily duplicates)
   
@@ -35,6 +42,10 @@ server <- function(input, output) {
   
   output$ex4 <- renderDataTable(
     Stradiaecdupfinal3, options = list(pageLength = 30)
+  )
+  
+  output$ex5 <- renderDataTable(
+    Otodomuadupfinal2, options = list(pageLength = 30)
   )
   
   
@@ -200,18 +211,59 @@ server <- function(input, output) {
   })
   
   
+  output$duplicatesPlot5 <- renderGvis({
+    
+    
+    graphotodomua <- gvisComboChart(dfotodomua, xvar="Date", 
+                                     yvar=c("%Duplicates", "Ads"),
+                                     options=list(title="%Duplicates by Active Ads",
+                                                  titleTextStyle="{color:'black',
+                                                fontName:'Courier',
+                                                fontSize:16}",
+                                                  curveType="function", 
+                                                  pointSize=9,
+                                                  seriesType="bars",
+                                                  series="[{type:'line', 
+                                                targetAxisIndex:0,
+                                                color:'blue'}, 
+                                                {type:'bars', 
+                                                targetAxisIndex:1,
+                                                color:'orange'}]",
+                                                  vAxes="[{title:'Percent',
+                                                format:'#,###%',
+                                                titleTextStyle: {color: 'black'},
+                                                textStyle:{color: 'black'},
+                                                textPosition: 'out',
+                                                minValue:0}, 
+                                                {title:'Thousands',
+                                                format:'#,###',
+                                                titleTextStyle: {color: 'black'},
+                                                textStyle:{color: 'black'},
+                                                textPosition: 'out',
+                                                minValue:0}]",
+                                                  hAxes="[{title:'Date',
+                                                textPosition: 'out'}]",
+                                                  width=1200, height=600
+                                     ))
+    
+    graphotodomua
+    
+  })
  
 }
 
 
 ui <- navbarPage(
   title = (""),
-  tabPanel('Overview',titlePanel("Stradia Latam Duplicated"),
+  tabPanel('Overview',titlePanel("Stradia Latam and Otodom UA Duplicated"),
            mainPanel(
-             h5("This report provides data about ad duplications and deleted for Stradia Latam (Argentina, Colombia, Peru and Ecuador) based on some variables."),
+             h5("This report provides data about ad duplications for Stradia Latam (Argentina, Colombia, Peru and Ecuador) and Otodom UA based on some variables."),
              br(),
-             h5("To find ad duplicates we use the following variables: 
+             h5("To find ad duplicates we use the following variables for Stradia Latam: 
                 Same user id, same brand, same model, same year, same fuel and mileage."),
+             br(),
+             h5("To find ad duplicates we use the following variables for Otodom UA: 
+                Same user id, same city id, same category id, same price and title similarity with at least 70%."),
              br(),
              h5("We have the plots with the relation between active ads and ad duplicates, by day. 
                 Due performance and memory capacity reasons, we just consider a maximum of 30 days per plot. With this we can have an overall perspective regarding the evolution of duplicates."), 
@@ -229,7 +281,9 @@ ui <- navbarPage(
   tabPanel('Stradia Pe Dup Graph', htmlOutput("duplicatesPlot3")),   
   tabPanel('Stradia Pe Table', dataTableOutput('ex3')),
   tabPanel('Stradia Ec Dup Graph', htmlOutput("duplicatesPlot4")),   
-  tabPanel('Stradia Ec Table', dataTableOutput('ex4'))
+  tabPanel('Stradia Ec Table', dataTableOutput('ex4')),
+  tabPanel('Otodom UA Dup Graph', htmlOutput("duplicatesPlot5")),   
+  tabPanel('Otodom UA Table', dataTableOutput('ex5'))
   
              )
 
