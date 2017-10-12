57ec2ee0915a350c2abf3eaa09f9f571e70a265f
danielrochaolx <daniel.rocha@olx.com>
2017-10-12 15:45:48 +0000

    ..

--- scripts/r/7.ETL/ETL_RDL_ads.R
+++ scripts/r/7.ETL/ETL_RDL_ads.R
@@ -17,98 +17,104 @@ args <- (commandArgs(TRUE))
 ## First check to see if arguments are passed.
 ## Then cycle through each element of the list and evaluate the expressions.
 if(length(args)==0){
-  print("No arguments supplied.")
-  ##supply default values
-  vertical <-""
+print("No arguments supplied.")
+##supply default values
+vertical <-"OtomotoPL"
 }else{
-  for(i in 1:length(args)){
-    eval(parse(text=args[[i]]))
-    #print(args[[i]])
-  }
+for(i in 1:length(args)){
+  eval(parse(text=args[[i]]))
+  #print(args[[i]])
+}
 }
 
 print(vertical)
 
 # select vertical -------------------------------------------------------------
 if(!exists("vertical")){
-  vertical <- "xvz"
+vertical <- "xvz"
 }
 
 dbUser <- get(paste0("cf", vertical, "DbUser")) 
 dbPass <- biUserPassword
 dbHost <- as.character(
-  ifelse(Sys.info()["nodename"] == "bisb", "127.0.0.1"
-         , get(paste0("cf", vertical, "DbHost")))
+ifelse(Sys.info()["nodename"] == "bisb", "127.0.0.1"
+       , get(paste0("cf", vertical, "DbHost")))
 )
 dbPort <- as.numeric(get(paste0("cf", vertical, "DbPort")))
 dbName <- get(paste0("cf", vertical, "DbName")) 
 
 
 # -----------------------------------------------------------------------------
-dates <- as.character(seq(as.Date("2017-03-01"), as.Date(Sys.Date()-1), "days"))
+dates <- as.character(seq(as.Date("2017-01-01"), as.Date(Sys.Date()-1), "days"))
 
-files <- list.files(path = '/home/daniel.rocha/datalake/',
-                    pattern = paste0('^RDL.*', vertical,'.*','ads' ,'.*feather$'),
-                    full.names = TRUE
+files <- list.files(path = '/data/lake/',
+                  pattern = paste0('^RDL.*', vertical,'.*','_ads_2' ,'.*feather$'),
+                  full.names = TRUE
 )
 
 if(length(files) > 0){
-  existingdates <- str_match(files, 
-                             paste0("(RDL)",
-                                    "_(", 
-                                    vertical, 
-                                    ")_(",
-                                    "[a-z].*",
-                                    ")_(",
-                                    "[0-9\\-]{10})"
-                             )
-  )
-  dates <- dates[!(dates %in% existingdates)] 
+existingdates <- str_match(files, 
+                           paste0("(RDL)",
+                                  "_(", 
+                                  vertical, 
+                                  ")_(",
+                                  "[a-z].*",
+                                  ")_(",
+                                  "[0-9\\-]{10})"
+                           )
+)
+dates <- dates[!(dates %in% existingdates)] 
 }
 
 # start loop for every missing date -----------------------------------------
 
 for (i in dates){
-  
-  print(i)
-  filename <- 
-    paste0(
-      "/home/daniel.rocha/datalake/RDL_",
-      vertical, 
-      "_ads_", 
-      i, 
-      ".feather"
-    )
-  
-  print(filename)
-  
-  # connect to database  ------------------------------------------------------
-  conDB <-  
-    dbConnect(
-      RMySQL::MySQL(),
-      username = dbUser,
-      password = dbPass,
-      host = dbHost,
-      port = dbPort, 
-      dbname = dbName
-    )
-  
-  # get data ------------------------------------------------------------------
-  dbSqlQuery <-
-    paste(
-      "SELECT * FROM ads", 
-      "WHERE created_at_first", 
-      "BETWEEN '", i, " 00:00:00'", 
-      "AND '", i, " 23:59:59';"
-    )
-  
-  dbSqlQuery <-
-    dbGetQuery(conDB,dbSqlQuery)
-  
-  
-  # disconnect from database  -------------------------------------------------
-  dbDisconnect(conDB)
-  
-  # write file to disk --------------------------------------------------------
-  write_feather(x = dbSqlQuery, path = filename)
+
+print(i)
+filename <- 
+  paste0(
+    "/data/lake/RDL_",
+    vertical, 
+    "_ads_", 
+    i, 
+    ".feather"
+  )
+
+print(filename)
+
+# connect to database  ------------------------------------------------------
+conDB <-  
+  dbConnect(
+    RMySQL::MySQL(),
+    username = dbUser,
+    password = bi_team_pt_password,
+    host = dbHost,
+    port = dbPort, 
+    dbname = dbName
+  )
+
+# get data ------------------------------------------------------------------
+dbSqlQuery <-
+  paste(
+    "SELECT * FROM ads", 
+    "WHERE created_at_first", 
+    "BETWEEN '", i, " 00:00:00'", 
+    "AND '", i, " 23:59:59'",
+    "UNION ALL", 
+    "SELECT * FROM ads_archive", 
+    "WHERE created_at_first", 
+    "BETWEEN '", i, " 00:00:00'", 
+    "AND '", i, " 23:59:59'",
+    ";"
+  )
+
+dbSqlQuery <-
+  dbGetQuery(conDB,dbSqlQuery)
+
+
+# disconnect from database  -------------------------------------------------
+dbDisconnect(conDB)
+
+# write file to disk --------------------------------------------------------
+write_feather(x = dbSqlQuery, path = filename)
 }
