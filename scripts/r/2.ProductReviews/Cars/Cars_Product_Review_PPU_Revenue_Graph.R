2c3653cecf549d50e33e9b8089b9942dcd5e3f6e
danielrochaolx <daniel.rocha@olx.com>
2017-10-12 15:46:41 +0000

    update values

--- scripts/r/2.ProductReviews/Cars/Cars_Product_Review_PPU_Revenue_Graph.R
+++ scripts/r/2.ProductReviews/Cars/Cars_Product_Review_PPU_Revenue_Graph.R
@@ -15,8 +15,8 @@ bullet.graph <- function(bg.data, bg.title, bg.subtitle, maxgb){
   gg <- gg + geom_bar(aes(measure, high),  fill="gray90", stat="identity", width=0.5, alpha=0.2) 
   #gg <- gg + geom_bar(aes(measure, mean),  fill="blue", stat="identity", width=0.5, alpha=0.2) 
   #gg <- gg + geom_bar(aes(measure, target),   fill="gray90", stat="identity", width=0.5)
-  gg <- gg + geom_text(data=bg.data[6,],  aes(measure, maxgb, label="target"), color = "#0570b0", vjust = 3, hjust=1,  family="Open Sans")
-  gg <- gg + geom_text(data=bg.data[6,], aes(measure, maxgb, label="actual"), color = "#74a9cf", vjust = -2, hjust=1,  family="Open Sans")
+  gg <- gg + geom_text(data=bg.data[8,],  aes(measure, maxgb, label="target"), color = "#0570b0", vjust = 3, hjust=1,  family="Open Sans")
+  gg <- gg + geom_text(data=bg.data[8,], aes(measure, maxgb, label="actual"), color = "#74a9cf", vjust = -2, hjust=1,  family="Open Sans")
   gg <- gg + geom_text(aes(measure, target, label=paste0("", target)), color = "#0570b0", vjust = 3, hjust=0.5,  family="Open Sans")
   gg <- gg + geom_text(aes(measure, value, label=paste0("",value, "(", var, ")")), color = "#74a9cf", vjust = -2, hjust=0, family="Open Sans", fontface = "bold")
   gg <- gg + geom_bar(aes(measure, value), fill="#74a9cf",  stat="identity", width=0.2) 
@@ -46,22 +46,22 @@ bullet.graph <- function(bg.data, bg.title, bg.subtitle, maxgb){
 # Graph For Revenue -----------------------------------------------------------
 
 df <- data.frame(
-  measure=c("Otomoto.PL", "Autovit.RO", "Standvirtual.PT", "Stradia.IN", "Stradia LATAM", "TOTAL"),
-  high=c(20000, 20000, 20000, 20000, 20000, 20000),
-  mean=c(5000,5000, 5000, 5000, 5000,0),
-  low=c(2500, 2500, 2500, 2500, 2500,0), 
-  target=c(13580412, 1898774, 2559287,  533150, 1045000,19616623),
-  value=c(17075240, 2489709, 2931331,  980174,  961000,24437454)
+  measure=c("Otomoto.PL", "Autovit.RO", "Standvirtual.PT", "Stradia.IN", "Stradia LATAM", "Indonesia", "Philippines", "TOTAL"),
+  high=c(20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000),
+  mean=c(5000,5000, 5000, 5000, 5000,0,0,0),
+  low=c(2500, 2500, 2500, 2500, 2500,0,0,0), 
+  target=c(16477839, 2297125, 3075824,  681650, 1317000,  616510,  307619, 24773567),
+  value=c(20868154, 3022781, 3555924, 1254204, 1202000, 1274965,  227025, 31405053)
 )
 
 
 df$var <- percent(round(df$value/df$target-1,3))
 df$target <- round(df$target / 1000000,2)
 df$value <- round(df$value / 1000000, 2)
-df$high <- 35
+df$high <- 45
 
 
-gh_cars_revenue <- bullet.graph(df, "CARS - Revenue", "FYTD August/FY18 - M USD", 35)
+gh_cars_revenue <- bullet.graph(df, "CARS - Revenue", "FYTD September/FY18 - M USD", 45)
 
 # Graph for Paying Professional Users ----------------------------------------- 
 
@@ -70,8 +70,8 @@ df <- data.frame(
   high=c(25000, 25000, 25000, 25000, 25000, 25000),
   mean=c(5000,5000, 5000, 5000, 5000,0),
   low=c(2500, 2500, 2500, 2500, 2500,0), 
-  target=c(9230, 975, 2877, 1930, 773, 15785),
-  value=c(9443, 932, 2549, 3146, 485, 16555)
+  target=c(9290, 990, 2920, 1993,  800, 15993),
+  value=c(9651, 938, 2671, 3957, 496, 17713)
 )
 
 myNumCols <- which(unlist(lapply(df, is.numeric)))
@@ -79,4 +79,4 @@ df[(nrow(df) + 1), myNumCols] <- colSums(df[, myNumCols], na.rm=TRUE)
 
 df$var <- percent(round(df$value/df$target-1,3))
 
-gh_cars_ppu <- bullet.graph(df, "CARS - Paying Professional Users", "FYTD August/FY18 ", 25000)
+gh_cars_ppu <- bullet.graph(df, "CARS - Paying Professional Users", "FYTD September/FY18 ", 25000)
