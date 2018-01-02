2ac23af8cea208769558cbd42e93d1695bb94ebf
pedmatos <pedmigmatos@gmail.com>
2018-01-02 12:02:50 +0000

    Update Data

--- R/PM0006-RepliesFunnelCR/app.R
+++ R/PM0006-RepliesFunnelCR/app.R
@@ -18,7 +18,7 @@ server <- function(input, output) {
   load("funnel_total_stro_per.RData")
  
   
-  #Ajust similarity percentage (Shiny has some problems with that)
+  #Ajust percentage (Shiny has some problems with that)
   
 funnel_total_imo_per$"Overall Conversion" <- paste(round(funnel_total_imo_per$"Overall Conversion"*100,digits=1),"%",sep="")
 funnel_total_imo_per$"Step conversion" <- paste(round(funnel_total_imo_per$"Step conversion"*100,digits=1),"%",sep="")
@@ -33,18 +33,18 @@ funnel_total_stro_per$"Step conversion" <- paste(round(funnel_total_stro_per$"St
   
   #Tables(daily duplicates)
   
-  output$ex1 <- renderDataTable(
-    funnel_total_otpl_per, options = list(pageLength = 10)
-  )
-  
-  output$ex2 <- renderDataTable(
-   funnel_total_imo_per, options = list(pageLength = 10)
-  )
-  
-  output$ex3 <- renderDataTable(
-    funnel_total_stro_per, options = list(pageLength = 10)
-  )
-  
+  # output$ex1 <- renderDataTable(
+  #   funnel_total_otpl_per, options = list(pageLength = 10)
+  # )
+  # 
+  # output$ex2 <- renderDataTable(
+  #  funnel_total_imo_per, options = list(pageLength = 10)
+  # )
+  # 
+  # output$ex3 <- renderDataTable(
+  #   funnel_total_stro_per, options = list(pageLength = 10)
+  # )
+  # 
 
   #plot (evolution by month)
   
@@ -56,7 +56,7 @@ funnel_total_stro_per$"Step conversion" <- paste(round(funnel_total_stro_per$"St
     graphfunnelotpl <- gvisSankey(funnel_total_stro, from="origin", 
                  to="to",weight="Step conversion",
                  options=list(
-                   height=600,width=950,
+                   height=800,width=950,
                    sankey="{link: {color: { fill: 'lightblue' } },
                         node: { width: 30, 
                color: { fill: '#a61d4c' },
@@ -128,11 +128,11 @@ funnel_total_stro_per$"Step conversion" <- paste(round(funnel_total_stro_per$"St
 ui <- navbarPage(
   title = ("Replies Funnel Conversion Rate"),
   tabPanel('Otodom PL Funnel Graph', htmlOutput("funnelotplPlot")),   
-  tabPanel('Otodom PL Funnel Table', dataTableOutput('ex1')),
+  #tabPanel('Otodom PL Funnel Table', dataTableOutput('ex1')),
   tabPanel('Imovirtual Funnel Graph', htmlOutput("funnelimoPlot")),   
-  tabPanel('Imovirtual Funnel Table', dataTableOutput('ex2')),
-  tabPanel('Storia RO Funnel Graph', htmlOutput("funnelstroPlot")),
-  tabPanel('Storia RO Funnel Table',dataTableOutput('ex3'))
+  #tabPanel('Imovirtual Funnel Table', dataTableOutput('ex2')),
+  tabPanel('Storia RO Funnel Graph', htmlOutput("funnelstroPlot"))
+  #tabPanel('Storia RO Funnel Table',dataTableOutput('ex3'))
   
 )
 
