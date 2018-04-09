a1587ab0deb857b81586017705efd6d71ac744ed
Pedro Matos <pedmigmatos@gmail.com>
2018-04-09 01:09:12 +0100

    Update Data

--- R/PM0010-VASRODashboard/body/body_migration.R
+++ R/PM0010-VASRODashboard/body/body_migration.R
@@ -79,7 +79,7 @@ server_migration <- function(input, output, session) {
  
  # fluid row 1, kpi 1: Total VAS Volume
   output$totalvasBox <- renderValueBox({
-        valueBox("-1%", "Total B2C VAS Purchases (vs Last Week)",
+        valueBox("+13%", "Total B2C VAS Purchases (vs Last Week)",
            icon = icon("shopping-cart"),
        color = "orange")
  })
@@ -95,7 +95,7 @@ server_migration <- function(input, output, session) {
   
   # fluid row 1, kpi 3: B2C Funnel 
   output$b2cfunnelBox <- renderValueBox({
-    valueBox("37%",
+    valueBox("18%",
              "Overall B2C Payment Funnel",
              icon = icon("filter"),
              color = "green")
@@ -103,7 +103,7 @@ server_migration <- function(input, output, session) {
   
   # fluid row 1, kpi 4: Total Revenue 
   output$totalrevBox <- renderValueBox({
-    valueBox("+4%",
+    valueBox("+23%",
              "Total B2C VAS Revenue (vs Last Week)",
              icon = icon("money"),
              color = "purple")
