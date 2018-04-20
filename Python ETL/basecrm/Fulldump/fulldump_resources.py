705a04ab6eae960a4bdb98a3af844c803e7113b7
Andre-Matias <33873731+Andre-Matias@users.noreply.github.com>
2018-04-20 10:37:54 +0100

    Improvements to code to catch ServerError and try again

--- Python ETL/basecrm/Fulldump/fulldump_resources.py
+++ Python ETL/basecrm/Fulldump/fulldump_resources.py
@@ -70,14 +70,14 @@ def s3_fulldump_contacts(client,keyId,sKeyId,bucketName,path,country,category):
 	name = "/home/ubuntu/Reports/contacts_"
 	while 1:
 		
-		print("Getting page #" +str(aux))
-		
+		#If there is an error obtaining a page, try again
 		while True:
 			try:
+				print("Getting page #" +str(aux))
 				data = client.contacts.list(page = aux, per_page = 100)
 				break
 			except basecrm.errors.ServerError as err:
-				print("Error: " + str(err) + ". Trying again...")
+				print("Error: basecrm.errors.ServerError. Trying again...")
 
 		if len(data) > 0: empty = False
 		else:
