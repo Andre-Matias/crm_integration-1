ca1d6841b944748ca0b448cde6630f4873f9f520
danielrochaolx <daniel.rocha@olx.com>
2018-04-20 11:19:48 +0000

    adding dynamic extraction

--- scripts/r/3.Analysis/CARS-5583.VasDynamics/OtomotoPL/2.DU.0105-GetHydraSearchs.R
+++ scripts/r/3.Analysis/CARS-5583.VasDynamics/OtomotoPL/2.DU.0105-GetHydraSearchs.R
@@ -5,21 +5,46 @@ library("purrr")
 library("jsonlite")
 
 # config ----------------------------------------------------------------------
-options(scipen=999)
+options(scipen = 999)
 
 # load credentials file -------------------------------------------------------
 load("~/credentials.Rdata")
 load("~/GlobalConfig.Rdata")
+
 Sys.setenv("AWS_ACCESS_KEY_ID" = myS3key,
            "AWS_SECRET_ACCESS_KEY" = MyS3SecretAccessKey)
 
-# 
-rangeDate <- as.character(seq(as.Date('2018-02-21'),as.Date('2018-02-26'),by = 1))
+bucket_path <- "s3://pyrates-data-ocean/"
 
-# connect to silver ---------------------------------------------------------
+hosts <- c("/h/v-otomoto-web")
+
+# define range
+startDate <- "2017-10-01"
+endDate <- as.character(Sys.Date()-1)
+endDate <- "2017-10-02"
+
+rangeDate <- as.character(seq(as.Date(startDate),as.Date(endDate),by = 1))
+
+# getting file list -----------------------------------------------------------
+s3ExistingsObjects <- 
+  as.data.frame(
+    get_bucket(
+      bucket = bucket_path,
+      max = Inf, prefix = "datalake/otomotoPL/searches/v-otomoto-web/"
+    )
+  )
+
+# check existing files --------------------------------------------------------
+listExistingDates <-
+  as.Date(str_extract(s3ExistingsObjects$Key, "[0-9]{4}-[0-9]{2}-[0-9]{2}"))
+
+# dates to get ----------------------------------------------------------------
+listDatesToGet <-
+  as.character(rangeDate[!(rangeDate %in% as.character(listExistingDates))])
+
+# connect to silver -----------------------------------------------------------
 drv <- dbDriver("PostgreSQL")
 
-hosts <- c("/h/v-otomoto-web", "/h/v-standvirtual-web", "/h/v-autovit-web")
 for(host in hosts){
   for(i in rangeDate){
     
@@ -64,17 +89,20 @@ for(host in hosts){
     
     rm("dfRequestDB")
     
-    
     dfHydraResults$extra <- gsub("\"\\[", "\\[", dfHydraResults$extra)
     dfHydraResults$extra <- gsub("\\]\"", "\\]", dfHydraResults$extra)
     dfHydraResults$extra <- lapply(dfHydraResults$extra, function(x) paste0(x, collapse=""))
     dfHydraResults$a <- purrr::map(dfHydraResults$extra, jsonlite::validate)
     dfHydraResults <- dfHydraResults[dfHydraResults$a == TRUE, ]
     
+    print (paste(host, "=>", i, "=> started at:", Sys.time()), "Saving to AWS")
+    
     s3saveRDS(x = dfHydraResults,
               object = paste0("Results_", gsub("/h/", "", host), "_", i, ".RDS"),
               bucket = paste0("pyrates-data-ocean/datalake/otomotoPL/searches/", gsub("/h/", "", host))
     )
+
+    print (paste(host, "=>", i, "=> started at:", Sys.time()), "Saved to AWS")
   }
 }
 
