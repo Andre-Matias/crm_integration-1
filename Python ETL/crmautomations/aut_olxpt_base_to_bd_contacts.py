e45ea3f315255b4ed6320332d01a0836d6195417
Andre-Matias <33873731+Andre-Matias@users.noreply.github.com>
2018-04-27 11:31:39 +0100

    CRM Automations first release

---
+++ Python ETL/crmautomations/aut_olxpt_base_to_bd_contacts.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import basecrm
+import boto
+from boto.s3.key import Key
+from munch import *
+import simplejson as json
+from decimal import *
+import os
+import sys
+import gzip
+from datetime import datetime, timedelta
+import requests
+import time
+import psycopg2
+import numpy as np
+from boto.s3.connection import S3Connection, Bucket, Key
+from retry import retry
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger('logger')
+
+# COLOCAR FILTRO DIA 
+
+def checkS3FileExists(conf_file,bucket,path):
+	conf = json.load(open(conf_file))
+	key = conf['s3_key']
+	skey = conf['s3_skey']
+	conn = S3Connection(key, skey)
+	b = Bucket(conn, bucket)
+	found_file = 'false'
+
+	for x in b.list(prefix = path[1:]):
+		if(len(str(x)) > 0):
+			print(path)
+			found_file = 'true'
+			break
+
+	return found_file
+
+def getCopySql(schema, table, bucket, manifest, credentials):
+    return "COPY %(schema)s.%(table)s\n" \
+		"FROM '%(bucket)s'\n" \
+		"JSON AS '%(manifest)s'\n" \
+		"dateformat 'auto'\n" \
+		"timeformat 'YYYY-MM-DDTHH:MI:SS'\n" \
+		"gzip\n" \
+		"CREDENTIALS '%(credentials)s';" \
+		% {
+		'schema': schema,
+		'table': table,
+		'bucket': bucket,
+		'manifest': manifest,
+		'credentials': credentials
+	}
+
+def getChandraConnection(conf_file):
+	data = json.load(open(conf_file))
+	return psycopg2.connect(dbname=data['dbname'], host=data['host'], port=data['port'], user=data['user'], password=data['pass'])
+	
+def getS3Keys(conf_file):
+	data = json.load(open(conf_file))
+	return "aws_access_key_id=%(key)s;aws_secret_access_key=%(skey)s" \
+	% {'key': data['s3_key'],'skey': data['s3_skey']}
+
+def deletePreviousS3Files(conf_file, keyId, sKeyId):
+	print("Deleting S3 Olxpt Contacts Files")
+	conf = json.load(open(conf_file))
+
+	conn = S3Connection(keyId, sKeyId)
+	b = Bucket(conn, 'pyrates-data-ocean')
+	for x in b.list(prefix = 'renato-teixeira/contacts/olxpt/'):
+		x.delete()
+
+@retry(exceptions=Exception, delay=1, tries=10, logger=logger)			
+def s3_fulldump_contacts(client,keyId,sKeyId,bucketName,data_path,category,country):
+	
+	print("Getting contacts data")
+	#Iterate for everypage returned by the API
+	aux = 1
+	name = "./Reports/aut_olxpt_base_to_bd_contacts_"
+	while 1:
+		
+		data = client.contacts.list(page = aux, per_page = 100)
+
+		if len(data) > 0: empty = False
+		else:
+			print("Uploaded #" + str(aux) + " files to S3") 
+			return 1
+
+		#Write on local gz file
+		output = gzip.open(name + str(aux).zfill(10) + ".txt.gz", 'wb')
+
+		#Iterate the list of contacts
+		for contact_data in data:
+			if  str(datetime.strptime(contact_data.updated_at[:10], '%Y-%m-%d')) >= str(datetime.today().date() - timedelta(days=7)):
+				contact = Munch()
+				contact.id = contact_data.id
+				contact.email = contact_data.email
+				contact.phone = contact_data.phone
+				contact.mobile = contact_data.mobile
+				if 'Categoria' in contact_data.custom_fields:
+					contact.categoria = contact_data.custom_fields['Categoria']
+				else:
+					contact.categoria = ''
+				if 'NIF' in contact_data.custom_fields:
+					contact.nif = contact_data.custom_fields['NIF']
+				else:
+					contact.nif = ''
+				if 'User_ID' in contact_data.custom_fields:
+					contact.user_id = contact_data.custom_fields['User_ID']
+				else:
+					contact.user_id = ''
+				contact.country = country
+				contact.category = category
+				output.write(json.dumps(contact,use_decimal=True)+"\n")
+
+		#Close gz file		
+		output.close()
+
+		#Upload file to S3
+		localName = name + str(aux).zfill(10) + ".txt.gz"
+
+		fileName="aut_olxpt_base_to_bd_contacts_" + str(aux).zfill(10) + ".txt.gz"
+
+		full_key_name = os.path.join(data_path+"contacts/olxpt/", fileName)
+		conn = boto.connect_s3(keyId,sKeyId)
+		bucket = conn.get_bucket(bucketName)
+		k = bucket.new_key(full_key_name)
+		k.key=full_key_name
+
+		k.set_contents_from_filename(localName)
+		
+		#Remove local gz file
+		os.remove(localName)
+		
+		#Next page iterate
+		aux += 1
+			
+def loadFromS3toRedshift(conf_file,schema,category,country,bucketName,data_path,date,manifest_path):
+	conn = getChandraConnection(conf_file)
+	credentials = getS3Keys(conf_file)
+	cur = conn.cursor()
+	
+	if(checkS3FileExists(conf_file,bucketName,str(data_path) + 'contacts/olxpt/') == 'true'):
+		print('Loading...')
+		cur.execute(
+			getCopySql(
+				schema, \
+				'aut_olxpt_base_to_bd_contacts', #alterar tabelas na BD 'aux_olxpt_contacts'
+				's3://%(bucketName)s%(data_path)scontacts/olxpt/' \
+					% {
+					'bucketName':bucketName,
+					'date': date,
+					'data_path':data_path},
+				's3://%(bucketName)s%(manifest_path)saut_olxpt_contacts_sync_jsonpath.json' \
+					% {
+					'bucketName':bucketName,
+					'manifest_path':manifest_path
+					}, 
+				credentials
+			)
+		)
+		conn.commit()
+
+	#Close connection
+	cur.close()
+	conn.close()
+
+def main(conf_file):
+	base_api_token = json.load(open(conf_file))['base_api_token_olxpt'] 
+	#base_api_token = '81aef80f2a67ff2d70f0d905c15aa9fe5db3339f51a377370f585aa128ecc77f' # dev imotpt
+	schema = json.load(open(conf_file))['schema']
+	country = json.load(open(conf_file))['country_pt']
+	category = json.load(open(conf_file))['category_olxpt']
+	data_path = json.load(open(conf_file))['data_path']
+	keyId = json.load(open(conf_file))['s3_key']
+	sKeyId = json.load(open(conf_file))['s3_skey']
+	bucketName = json.load(open(conf_file))['bucketName']
+	manifest_path = json.load(open(conf_file))['manifest_path']
+	
+	client = basecrm.Client(access_token=base_api_token)
+	date = str(datetime.now().strftime('%Y/%m/%d/'))
+
+### TODO - DELETE S3 PATH BEFORE UNLOADING!!!!
+	deletePreviousS3Files(conf_file, keyId, sKeyId)
+	
+	s3_fulldump_contacts(client,keyId,sKeyId,bucketName,data_path,category,country)
+
+	loadFromS3toRedshift(conf_file,schema,category,country,bucketName,data_path,date,manifest_path)
+	
+if __name__ == "__main__":
+	conf_file = sys.argv[1] # File with source database
+	main(conf_file)
---
+++ Python ETL/crmautomations/full_load/aut_imopt_bd_to_base_deals_full.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import sys
+import csv, ast, psycopg2, json, basecrm, gzip
+from munch import * 
+import time
+import aut_imopt_base_to_bd_contacts_full
+import aut_imopt_base_to_bd_deals_full
+import threading
+from retry import retry
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger('logger')
+MAX_ACTIVE_THREADS = 5
+
+@retry(exceptions=Exception, delay=1, tries=10, logger=logger)	
+def updateDealsInBase(client, result_list):
+	#Update dos Deals
+	for result in result_list:
+		deal = Munch()
+		deal.id = result[1]
+		deal.custom_fields = Munch()
+		deal.custom_fields['NIF'] = result[2]	
+		deal.custom_fields['User ID'] = result[3] 	
+		deal.custom_fields['Designação Fiscal'] = result[4] 	
+		deal.custom_fields['Fiscal Address'] = result[5] 	
+		deal.custom_fields['Email Address'] = result[6] 	
+		deal.custom_fields['Natureza'] = result[7] 	
+		client.deals.update(deal.id, deal)
+
+def getDatabaseConnection(conf_file):
+	data = json.load(open(conf_file))
+	return psycopg2.connect(dbname=data['dbname'], host=data['host'], port=data['port'], user=data['user'], password=data['pass'])
+
+conf_file = sys.argv[1] # File with source database
+
+base_api_token = json.load(open(conf_file))['base_api_token_imopt'] 
+
+client = basecrm.Client(access_token=base_api_token)
+
+conn = getDatabaseConnection(conf_file)
+cur = conn.cursor()
+
+print('Start Truncate aut_imopt_base_to_bd_contacts: ' + time.strftime("%H:%M:%S"))
+cur.execute("truncate table crm_integration_anlt.aut_imopt_base_to_bd_contacts; ")
+print('End Truncate aut_imopt_base_to_bd_contacts: ' + time.strftime("%H:%M:%S"))
+
+print('Start Truncate aut_imopt_base_to_bd_deals: ' + time.strftime("%H:%M:%S"))
+cur.execute("truncate table crm_integration_anlt.aut_imopt_base_to_bd_deals; ")
+print('End Truncate aut_imopt_base_to_bd_deals: ' + time.strftime("%H:%M:%S"))
+
+aut_imopt_base_to_bd_contacts.main(conf_file)
+aut_imopt_base_to_bd_deals.main(conf_file)
+
+print('Starting Query: ' + time.strftime("%H:%M:%S"))
+
+#Contact Differences
+cur.execute(
+			"select "\
+              "aut_imopt_base_to_bd_contacts.contacts_id, "\
+              "aut_imopt_base_to_bd_contacts.deals_id, "\
+              "aut_imopt_base_to_bd_contacts.nif, "\
+              "aut_imopt_base_to_bd_contacts.user_id, "\
+              "aut_imopt_base_to_bd_contacts.designacao_fiscal, "\
+              "aut_imopt_base_to_bd_contacts.fiscal_address, "\
+              "aut_imopt_base_to_bd_contacts.email_address, "\
+              "aut_imopt_base_to_bd_contacts.natureza "\
+            "from "\
+              "( "\
+                "select "\
+                  "aut_imopt_base_to_bd_contacts.id contacts_id, "\
+                  "aut_imopt_base_to_bd_deals.id deals_id, "\
+                  "aut_imopt_base_to_bd_contacts.nif, "\
+                  "aut_imopt_base_to_bd_contacts.user_id, "\
+                  "aut_imopt_base_to_bd_contacts.designacao_fiscal, "\
+                  "contacts_address.fiscal_address, "\
+                  "aut_imopt_base_to_bd_contacts.email_address, "\
+                  "replace(replace(replace(aut_imopt_base_to_bd_contacts.natureza,'\\\"',''),'[',''),']','') as natureza "\
+                "from "\
+                  "crm_integration_anlt.aut_imopt_base_to_bd_contacts, "\
+                  "crm_integration_anlt.aut_imopt_base_to_bd_deals, "\
+                  "( "\
+                    "select id, replace(replace(replace(listagg(address_value, ', ') within group (order by id, cod_address_name),', null',''),'null, ',''),'null',' ') fiscal_address "\
+                    "from ( "\
+                    "select "\
+                      "id, "\
+                      "fiscal_address, "\
+                      "case when (case when segment = '{}' then null else replace(replace(split_part(segment,'\\\":\\\"',1),'{\\\"',''),'\\\"}','') end) = 'line1' then 1 "\
+                      "when (case when segment = '{}' then null else replace(replace(split_part(segment,'\\\":\\\"',1),'{\\\"',''),'\\\"}','') end) = 'postal_code' then 2 "\
+                      "when (case when segment = '{}' then null else replace(replace(split_part(segment,'\\\":\\\"',1),'{\\\"',''),'\\\"}','') end) = 'state' then 3 "\
+                      "when (case when segment = '{}' then null else replace(replace(split_part(segment,'\\\":\\\"',1),'{\\\"',''),'\\\"}','') end) = 'city' then 4 "\
+                      "when (case when segment = '{}' then null else replace(replace(split_part(segment,'\\\":\\\"',1),'{\\\"',''),'\\\"}','') end) = 'country' then 5 "\
+                      "end cod_address_name, "\
+                      "case when segment = '{}' then null else replace(replace(split_part(segment,'\\\":\\\"',1),'{\\\"',''),'\\\"}','') end address_name, "\
+                      "case when segment = '{}' then null else replace(replace(split_part(segment,'\\\":\\\"',2),'{\\\"',''),'\\\"}','') end address_value "\
+                    "from "\
+                      "( "\
+                        "select "\
+                          "ts.id, "\
+                          "ts.fiscal_address, "\
+                          "s.gen_num, "\
+                          "split_part(replace(replace(replace(replace(replace(fiscal_address,':null',':\\\"null\\\"'),':false,',':\\\"false\\\",'),':true,',':\\\"true\\\",'),':false}',':\\\"false\\\"}'),':true}',':\\\"true\\\"}'),'\\\",\\\"', s.gen_num) AS segment "\
+                        "from "\
+                          "crm_integration_anlt.aut_imopt_base_to_bd_contacts ts, "\
+                          "( "\
+                          "select "\
+                            "* "\
+                          "from "\
+                            "( "\
+                              "select (1000 * t1.num) + (100 * t2.num) + (10 * t3.num) + t4.num AS gen_num "\
+                              "from "\
+                              "(select 1 as num union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9 union select 0) t1, "\
+                              "(select 1 as num union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9 union select 0) t2, "\
+                              "(select 1 as num union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9 union select 0) t3, "\
+                              "(select 1 as num union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9 union select 0) t4 "\
+                            ") "\
+                          "where "\
+                          "gen_num between 1 and (select max(regexp_count(replace(fiscal_address,':null',':\\\"null\\\"'), '\\\",\\\"') + 1) from crm_integration_anlt.aut_imopt_base_to_bd_contacts /*where id = 117058292*/) "\
+                          ") s "\
+                        "where "\
+                          "split_part(replace(fiscal_address,':null',':\\\"null\\\"'), '\\\",\\\"', s.gen_num) != '' "\
+                          "and fiscal_address != '{}' "\
+                      ") "\
+                    "order by 1,3) "\
+                    "group by id "\
+                    ") contacts_address "\
+                "where  "\
+                  "aut_imopt_base_to_bd_contacts.id = aut_imopt_base_to_bd_deals.contacts_id "\
+                "and aut_imopt_base_to_bd_contacts.id = contacts_address.id (+) "\
+				#"and aut_imopt_base_to_bd_contacts.id = 200682693 "\
+				#"and aut_imopt_base_to_bd_deals.id in(49245232,49245233) "\
+              ") aut_imopt_base_to_bd_contacts, "\
+              "crm_integration_anlt.aut_imopt_base_to_bd_deals "\
+            "where "\
+              "aut_imopt_base_to_bd_contacts.contacts_id = aut_imopt_base_to_bd_deals.contacts_id (+) "\
+              "and aut_imopt_base_to_bd_contacts.deals_id = aut_imopt_base_to_bd_deals.id (+) "\
+              "and aut_imopt_base_to_bd_contacts.nif = aut_imopt_base_to_bd_deals.nif (+) "\
+              "and aut_imopt_base_to_bd_contacts.user_id = aut_imopt_base_to_bd_deals.user_id (+) "\
+              "and aut_imopt_base_to_bd_contacts.designacao_fiscal = aut_imopt_base_to_bd_deals.designacao_fiscal (+) "\
+              "and aut_imopt_base_to_bd_contacts.fiscal_address = aut_imopt_base_to_bd_deals.fiscal_address (+) "\
+              "and aut_imopt_base_to_bd_contacts.email_address = aut_imopt_base_to_bd_deals.email_address (+) "\
+              "and aut_imopt_base_to_bd_contacts.natureza = aut_imopt_base_to_bd_deals.natureza (+) "\
+              "and aut_imopt_base_to_bd_deals.id is null;")
+result_list = cur.fetchall()
+
+print('End Query: ' + time.strftime("%H:%M:%S"))
+
+print('Starting Updating: ' + time.strftime("%H:%M:%S"))
+
+# Threading implementation
+number_active_threads = 0
+number_deals = len(result_list)
+deals_per_thread = - (-number_deals // MAX_ACTIVE_THREADS) # Ceiling of integer division
+thread_list = []
+
+i = 0
+j = deals_per_thread
+for n in range(0, MAX_ACTIVE_THREADS):
+	t = threading.Thread(target=updateDealsInBase, args=(client, result_list[i:j]))
+	thread_list.append(t)
+	t.start()
+	print('Spawned thread #' + str(n+1))
+	i = i + deals_per_thread
+	j = j + deals_per_thread
+	if j > number_deals:
+		j = number_deals
+
+for t in thread_list:
+	t.join()
+
+print('End of Updating: ' + time.strftime("%H:%M:%S"))
+
+cur.close()
+conn.close()
