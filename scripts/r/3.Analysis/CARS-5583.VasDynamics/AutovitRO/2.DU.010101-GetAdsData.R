d2f7a8aca4ebd0ddb99630750bb2cc687bfbad07
danielrochaolx <daniel.rocha@olx.com>
2018-05-07 09:38:14 +0000

    files for AutovitRO

---
+++ scripts/r/3.Analysis/CARS-5583.VasDynamics/AutovitRO/2.DU.010101-GetAdsData.R
@@ -0,0 +1,120 @@
+# load libraries --------------------------------------------------------------
+library("aws.s3")
+library("data.table")
+library("stringr")
+library("RMySQL")
+library("slackr")
+
+
+# load credentials ------------------------------------------------------------
+load("~/GlobalConfig.Rdata")
+load("~/credentials.Rdata")
+slackrSetup()
+
+# config ----------------------------------------------------------------------
+bucket_path <- "s3://pyrates-data-ocean/"
+
+Sys.setenv("AWS_ACCESS_KEY_ID" = myS3key,
+           "AWS_SECRET_ACCESS_KEY" = MyS3SecretAccessKey)
+
+vertical <- "autovitRO"
+file <- "ads"
+
+startDate <- "2017-01-01"
+endDate <- as.character(Sys.Date()-1)
+
+AllDates <- seq.Date(as.Date(startDate), as.Date(endDate), 1)
+
+# getting file list -----------------------------------------------------------
+s3ExistingsObjects <- 
+  as.data.frame(
+    get_bucket(
+      bucket = bucket_path,
+      max = Inf, prefix = paste(sep = "/", "datalake", vertical, file, "RDS")
+    )
+  )
+
+# check existing files --------------------------------------------------------
+
+listExistingDates <-
+  as.Date(str_extract(s3ExistingsObjects$Key, "[0-9]{4}-[0-9]{2}-[0-9]{2}"))
+
+# dates to get ----------------------------------------------------------------
+
+listDatesToGet <-
+  as.character(AllDates[!(AllDates %in% listExistingDates)])
+
+# get files ----------
+
+for (i in listDatesToGet) {
+filename <- paste0("/datalake/", vertical,"/", file, "/", "RDS/",
+                   "RDL_",vertical, "_", file ,"_",  i, ".RDS")
+print(filename)
+
+# connect to database  ------------------------------------------------------
+conDB <-  
+dbConnect(
+RMySQL::MySQL(),
+username = "bi_team_pt",
+password = bi_team_pt_password,
+host = "127.0.0.1",
+port = 3315, 
+dbname = "autovitro"
+)
+
+# get data ------------------------------------------------------------------
+dbSqlQuery1 <-
+paste(
+"SELECT * , 'ads' AS tablename ",
+"FROM autovitro.ads",
+"WHERE created_at_first >= '", i, "00:00:00' AND created_at_first <= '", i,"23:59:59'",
+"COLLATE latin1_general_ci"
+)
+
+dbSqlQuery2 <-
+  paste(
+"SELECT *, 'ads_archive' AS tablename",
+"FROM autovitro.ads_archive",
+"WHERE created_at_first >= '", i, "00:00:00' AND created_at_first <= '", i,"23:59:59'",
+"COLLATE latin1_general_ci;"
+)
+
+dfSqlQuery1 <-
+  dbGetQuery(conDB, dbSqlQuery1)
+
+dfSqlQuery2 <-
+  dbGetQuery(conDB, dbSqlQuery2)
+
+# disconnect from database  -------------------------------------------------
+dbDisconnect(conDB)
+
+# fix columns with switched values region_id e category_id ------------------
+
+dfSqlQuery <- rbind(dfSqlQuery1, dfSqlQuery2)
+## create tmp columns ----
+dfSqlQuery$region_id_tmp <- as.numeric(0)
+dfSqlQuery$category_id_tmp <- as.numeric(0)
+
+dfSqlQuery$category_id_tmp[dfSqlQuery$tablename =="ads_archive"] <-
+dfSqlQuery$region_id[dfSqlQuery$tablename =="ads_archive"]
+
+dfSqlQuery$region_id_tmp[dfSqlQuery$tablename =="ads_archive"] <-
+dfSqlQuery$category_id[dfSqlQuery$tablename =="ads_archive"] 
+
+dfSqlQuery$category_id[dfSqlQuery$tablename =="ads_archive"] <-
+dfSqlQuery$category_id_tmp[dfSqlQuery$tablename =="ads_archive"]
+
+dfSqlQuery$region_id[dfSqlQuery$tablename =="ads_archive"] <-
+dfSqlQuery$region_id_tmp[dfSqlQuery$tablename =="ads_archive"]
+
+## delete tmp colums
+dfSqlQuery$region_id_tmp <- NULL
+dfSqlQuery$category_id_tmp <- NULL
+
+# write file to disk --------------------------------------------------------
+s3saveRDS(
+  x = dfSqlQuery, 
+  bucket = bucket_path, 
+  object = filename
+)
+}
