fedf6ce2590a54cca798ce665e76711eeb6110d4
Andre-Matias <33873731+Andre-Matias@users.noreply.github.com>
2018-05-25 16:22:41 +0100

    Corrections to Revenue calculations for IMO and STV

--- Python ETL/crmintegration/4_analytical_to_base/OTMPL_KPIs.sql
+++ Python ETL/crmintegration/4_analytical_to_base/OTMPL_KPIs.sql
@@ -1771,6 +1771,7 @@ create table crm_integration_anlt.tmp_pl_otomoto_calc_revenue_0 as
 						and g.cod_source_system = c.cod_source_system
 						and g.valid_to = 20991231
 						and h.opr_source_system = a.livesync_dbname
+						and a.livesync_dbname = b.livesync_dbname
 						and h.cod_source_system = c.cod_source_system
 						and date_trunc('month',b.last_status_date) = date_trunc('month',sysdate)
 					group by
@@ -1850,6 +1851,7 @@ create table crm_integration_anlt.tmp_pl_otomoto_calc_revenue_1 as
 						and g.cod_source_system = c.cod_source_system
 						and g.valid_to = 20991231
 						and h.opr_source_system = a.livesync_dbname
+						and a.livesync_dbname = b.livesync_dbname
 						and h.cod_source_system = c.cod_source_system
 						and date_trunc('month',b.last_status_date) = date_trunc('month',add_months(sysdate,-1))
 					group by
