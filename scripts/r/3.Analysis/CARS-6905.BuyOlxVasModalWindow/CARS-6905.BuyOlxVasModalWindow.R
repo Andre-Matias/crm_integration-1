dc71604df3da066d1b9dadec026d28f23b588101
danielrochaolx <daniel.rocha@olx.com>
2018-05-25 16:21:41 +0100

    create extracting loop for mixpanel

--- scripts/r/3.Analysis/CARS-6905.BuyOlxVasModalWindow/CARS-6905.BuyOlxVasModalWindow.R
+++ scripts/r/3.Analysis/CARS-6905.BuyOlxVasModalWindow/CARS-6905.BuyOlxVasModalWindow.R
@@ -45,3 +45,58 @@ listMixpanelAccounts <-
 print(paste(Sys.time(), "Define Start/End Date"))
 startDate <- as.character("2018-05-21")
 endDate <- as.character(Sys.Date())
+
+dfTmp <- as_tibble()
+
+for(i in listMixpanelAccounts){
+  
+  dfChunk <- as_tibble()
+  
+  jqlQuery <- 
+    '
+    function main() {{
+      return Events({{
+      from_date: "{startDate}",
+      to_date:   "{endDate}"
+    }})
+      .filter((e) => {{
+      return (
+      e.name == "my_ads_1_click_vas_modal"
+      || e.name == "ab_test_my_ads_1_click_vas_modal_confirm"
+      )
+      && e.properties.experiments
+      && e.properties.experiments.toString().indexOf("{idExperiment}") > -1
+      && (
+      !e.properties.$referring_domain
+      || (e.properties.$referring_domain && e.properties.$referring_domain.includes("fixeads")) === false
+      )
+      }})
+      .groupByUser(["name"],
+      (state, events) => {{
+      var firstEvent = events.length  ? events[0].properties : null; 
+      return [firstEvent. buy_vas_olx]
+      }})
+      }};
+    '
+  
+  print(paste(Sys.time(), "Sending JQL Query", i[[1]]))
+  idExperiment <- as.character(i[[3]])
+  
+  jqlQuery <- glue(jqlQuery)
+  
+  dfChunk <- 
+    as_tibble(
+      mixpanelJQLQuery(account = i[[2]], 
+                       jqlQuery
+      )
+    )
+  
+  dfChunk$project <- as.character(i[[1]])
+  
+  
+  if(nrow(dfTmp) == 0){
+    dfTmp <- dfChunk
+  } else {
+    dfTmp <- rbind(dfTmp, dfChunk)
+  }
+}
