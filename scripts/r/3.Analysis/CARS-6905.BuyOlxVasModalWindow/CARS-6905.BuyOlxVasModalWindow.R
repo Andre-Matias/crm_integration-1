c03cb6c9bde3c98a2cd074e9d55b6d5de48630d3
danielrochaolx <daniel.rocha@olx.com>
2018-05-25 16:01:21 +0100

    adding configs

--- scripts/r/3.Analysis/CARS-6905.BuyOlxVasModalWindow/CARS-6905.BuyOlxVasModalWindow.R
+++ scripts/r/3.Analysis/CARS-6905.BuyOlxVasModalWindow/CARS-6905.BuyOlxVasModalWindow.R
@@ -9,3 +9,39 @@
 # Standvirtual Experiment ID 	10682743623 
 # Original (ID: 10676043240)
 # Variation #1 (ID: 10675063532)
+
+print(paste(Sys.time(), "Loading libraries"))
+# load libraries --------------------------------------------------------------
+library("RMixpanel")
+library("dplyr")
+library("data.table")
+library("dtplyr")
+library("magrittr")
+library("tidyr")
+library("anytime")
+library("ggthemes")
+library("showtext")
+library("glue")
+library("aws.s3")
+
+print(paste(Sys.time(), "Loading credentials"))
+# load mixpanel user's credentials --------------------------------------------
+load("~/credentials.Rdata")
+load("~/GlobalConfig.Rdata")
+
+Sys.setenv("AWS_ACCESS_KEY_ID" = myS3key,
+           "AWS_SECRET_ACCESS_KEY" = MyS3SecretAccessKey)
+
+font_add_google("Open Sans", "opensans")
+
+print(paste(Sys.time(), "Loading list of accounts"))
+
+listMixpanelAccounts <- 
+  list(otomotopl = list("OtomotoPL", mixpanelOtomotoAccount, "10680473891"),
+       autovitro = list("AutovitRO", mixpanelAutovitAccount, "10678587448"),
+       standvirtualpt = list("StandvirtualPT", mixpanelStandvirtualAccount, "10682743623")
+  )
+
+print(paste(Sys.time(), "Define Start/End Date"))
+startDate <- as.character("2018-05-21")
+endDate <- as.character(Sys.Date())
