7251dc12d47da6064a1d1e4531a805fd1d30e9ed
danielrochaolx <daniel.rocha@olx.com>
2018-05-25 15:50:28 +0100

    initial script with requirements

---
+++ scripts/r/3.Analysis/CARS-6905.BuyOlxVasModalWindow/CARS-6905.BuyOlxVasModalWindow.R
@@ -0,0 +1,11 @@
+# Otomoto Experiment ID 	10680473891
+# Original (ID: 10693100212)
+# Variation #1 (ID: 10677843892)
+# 
+# Autovit Experiment ID 	10678587448 
+# Original (ID: 10684616469)
+# Variation #1 (ID: 10690060574)
+# 
+# Standvirtual Experiment ID 	10682743623 
+# Original (ID: 10676043240)
+# Variation #1 (ID: 10675063532)
