6656b32c993da8c15c6a48e34cb3a3a73da3981a
Andre-Matias <33873731+Andre-Matias@users.noreply.github.com>
2018-06-07 14:45:41 +0100

    Fixing bug in Auto Field Sync Automation
    Field last_stage_change_at was not being sent in right date format

--- Python ETL/crmautomations/aut_olxpt_bd_to_base_deals.py
+++ Python ETL/crmautomations/aut_olxpt_bd_to_base_deals.py
@@ -69,7 +69,7 @@ cur.execute(
               "aut_olxpt_base_to_bd_contact.phone, "\
               "aut_olxpt_base_to_bd_contact.user_id, "\
               "left(replace(replace(aut_olxpt_base_to_bd_contact.last_stage_change_at,'T',' '),'Z',''),10) as last_stage_change_at, "\
-              "case when aut_olxpt_base_to_bd_contact.payment_date = '' then to_char(tO_date(left(replace(replace(aut_olxpt_base_to_bd_contact.last_stage_change_at,'T',' '),'Z',''),10),'yyyy-mm-dd'),'dd/mm/yyyy') else aut_olxpt_base_to_bd_contact.payment_date end payment_date "\
+              "case when aut_olxpt_base_to_bd_contact.payment_date = '' then to_char(tO_date(left(replace(replace(aut_olxpt_base_to_bd_contact.last_stage_change_at,'T',' '),'Z',''),10),'yyyy-mm-dd'),'yyyy-mm-dd') else aut_olxpt_base_to_bd_contact.payment_date end payment_date "\
             "from "\
               "( "\
                 "select "\
