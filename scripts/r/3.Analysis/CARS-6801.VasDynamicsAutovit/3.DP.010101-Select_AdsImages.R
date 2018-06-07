259f9274ae17d2b8e58cd7fd3b01bb3a8f8f4ce1
danielrochaolx <daniel.rocha@olx.com>
2018-06-07 11:25:47 +0000

    load data files

---
+++ scripts/r/3.Analysis/CARS-6801.VasDynamicsAutovit/3.DP.010101-Select_AdsImages.R
@@ -0,0 +1,33 @@
+# load libraries --------------------------------------------------------------
+library("aws.s3")
+library("magrittr")
+library("dplyr")
+library("data.table")
+library("dtplyr")
+library("readr")
+library("stringr")
+library("tidyr")
+library("ggplot2")
+
+# load credentials ------------------------------------------------------------
+load("~/GlobalConfig.Rdata")
+load("~/credentials.Rdata")
+
+vertical <- "autovitRO"
+
+Sys.setenv("AWS_ACCESS_KEY_ID" = myS3key,
+           "AWS_SECRET_ACCESS_KEY" = MyS3SecretAccessKey)
+
+origin_bucket_path <- "s3://pyrates-data-ocean/"
+origin_bucket_prefix <- "datalake/autovitRO/AIO/"
+
+dfTargets <-
+  as_tibble(
+    s3readRDS(object = paste0(origin_bucket_prefix, "Targets_AIO.RDS"), bucket = origin_bucket_path)
+  )
+
+dfAdsImages <-
+  as_tibble(
+    s3readRDS(object = paste0(origin_bucket_prefix, "autovitRO_adsimages_AIO.RDS"), bucket = origin_bucket_path)
+  )
+
