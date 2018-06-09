9838def6b2b1ff14b668257183379f793b76fc5f
danielrochaolx <daniel.rocha@olx.com>
2018-06-09 15:38:33 +0000

    clear trash and tidy df

--- scripts/r/3.Analysis/CARS-6801.VasDynamicsAutovit/2.DU.010101-GetAdsData_AIO.R
+++ scripts/r/3.Analysis/CARS-6801.VasDynamicsAutovit/2.DU.010101-GetAdsData_AIO.R
@@ -16,6 +16,9 @@ load("~/GlobalConfig.Rdata")
 load("~/credentials.Rdata")
 slackrSetup()
 
+#clear garbage
+rm(list=setdiff(ls(), c("myS3key","MyS3SecretAccessKey")))
+
 # config ----------------------------------------------------------------------
 bucket_path <- "s3://pyrates-data-ocean/"
 
@@ -43,7 +46,21 @@ dat_list <-
 
 # merge all data frames from the list to a single data frame ------------------
 dat <-
-  rbindlist(dat_list, use.names = TRUE, fill = TRUE)
+  as_tibble(rbindlist(dat_list, use.names = TRUE, fill = TRUE))
+
+# select 
+
+dat <-
+  dat %>%
+  filter(category_id %in% c(29, 81), net_ad_counted == 1) %>%
+  mutate(created_at_first_day = as.Date(created_at_first),
+         ad_id = as.character(id)
+  ) %>%
+  select(-created_at_first, -id) %>%
+  filter(created_at_first_day >= as.Date('2017-10-01')) %>%
+  select(ad_id, created_at_first_day)
+
+# save to aws
 
  s3saveRDS(
    x = dat, 
