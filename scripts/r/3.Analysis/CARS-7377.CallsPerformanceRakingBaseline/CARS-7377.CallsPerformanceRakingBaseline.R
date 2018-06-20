55a8f4d91fa2cd1384262946876e155f57a3ea4e
danielrochaolx <daniel.rocha@olx.com>
2018-06-20 10:53:10 +0000

    refactor to get all countries

--- scripts/r/3.Analysis/CARS-7377.CallsPerformanceRakingBaseline/CARS-7377.CallsPerformanceRakingBaseline.R
+++ scripts/r/3.Analysis/CARS-7377.CallsPerformanceRakingBaseline/CARS-7377.CallsPerformanceRakingBaseline.R
@@ -1,25 +1,31 @@
+library("data.table")
+library("dplyr")
+library("dtplyr")
+library("magrittr")
 
 #"USE calltrackingdb; SELECT service_id, external_user_id, external_sub_user_id, call_result, COUNT(*) FROM call_statistics WHERE service_id = 1 GROUP BY 1,2,3,4;"
+# "USE calltrackingdb; SELECT * FROM call_statistics" > dump_call_statistics.txt
 
 dfRawCallStatistics <-
-  as_tibble(read.table("~/dump.txt", header = TRUE, sep = "\t", col.names = c("service_id", "external_user_id", "external_sub_user_id", "call_result", "Qty")))
+  as_tibble(read.table("~/dump_call_statistics.txt", header = TRUE, sep = "\t"))
 
-dfRawCallStatistics$Qty <- as.numeric(dfRawCallStatistics$Qty)
-dfRawCallStatistics$external_sub_user_id <- as.character(dfRawCallStatistics$external_sub_user_id)
+dfRawService <-
+  as_tibble(read.table("~/dump_service.txt", header = TRUE, sep = "\t"))
 
 Stats <-
   dfRawCallStatistics %>%
   filter(!grepl("test", tolower(external_sub_user_id)))%>%
-  group_by(external_user_id, call_result) %>%
-  summarise(qtyByType = sum(Qty)) %>%
-  arrange(external_user_id) %>%
-  group_by(external_user_id) %>%
+  group_by(service_id, external_user_id, call_result) %>%
+  summarise(qtyByType = sum(n())) %>%
+  arrange(service_id, external_user_id) %>%
+  group_by(service_id, external_user_id) %>%
   mutate(qtyTotal = sum(qtyByType),
          perQtyType = qtyByType / qtyTotal,
          segment = cut(perQtyType, breaks = c(0, 0.71, 0.80, 0.90, 1), include.lowest = TRUE, labels = c("Poor", "Medium", "Good", "Excellent"))) %>%
   filter(call_result == "ANSWERED") %>%
-  group_by(segment) %>%
+  group_by(service_id, segment) %>%
   summarise(qtyUsers = sum(n())) %>%
-  mutate(perUsers = scales::percent(qtyUsers / sum(qtyUsers)))
-
-  
+  mutate(perUsers = scales::percent(qtyUsers / sum(qtyUsers))) %>%
+  inner_join(dfRawService[, c("service_id", "website_name")], by = c("service_id")) %>%
+  group_by() %>%
+  select("website_name", "segment", "qtyUsers", "perUsers")
