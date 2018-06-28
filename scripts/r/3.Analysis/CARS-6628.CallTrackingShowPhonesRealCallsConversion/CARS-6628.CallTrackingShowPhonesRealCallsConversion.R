331365680488cee184ffe6072ae5f2beb3984a85
danielrochaolx <daniel.rocha@olx.com>
2018-06-28 15:56:36 +0100

    done

--- scripts/r/3.Analysis/CARS-6628.CallTrackingShowPhonesRealCallsConversion/CARS-6628.CallTrackingShowPhonesRealCallsConversion.R
+++ scripts/r/3.Analysis/CARS-6628.CallTrackingShowPhonesRealCallsConversion/CARS-6628.CallTrackingShowPhonesRealCallsConversion.R
@@ -5,12 +5,11 @@ library("magrittr")
 library("glue")
 library("RMySQL")
 library("RPostgreSQL")
+library("corrplot")
 
 # load credentials ------------------------------------------------------------
 load("~/GlobalConfig.Rdata")
 load("~/credentials.Rdata")
-slackrSetup()
-
 
 # get data from call tracking service
 
@@ -20,6 +19,7 @@ dfRawCallStatistics <-
 dfRawService <-
   read.table("~/dump_service.txt", header = TRUE, sep = "\t")
 
+# join to get service name 
 dfRaw <- 
   dfRawCallStatistics %>%
   left_join(dfRawService, by = c("service_id"))
@@ -58,7 +58,6 @@ strStandvirtualUserIDs <- gsub("[^0-9]\\,", "", strStandvirtualUserIDs)
 sqlQueryOtomotoPL <- 
   glue("SELECT id, user_id, status FROM ads WHERE user_id IN ({strOtomotoPLUserIDs}) AND net_ad_counted = 1")
 
-
 sqlQueryStandvirtual <- 
   glue("SELECT id, user_id, status FROM ads WHERE user_id IN ({strStandvirtualUserIDs}) AND net_ad_counted = 1")
 
@@ -67,15 +66,15 @@ conDB <-
     RMySQL::MySQL(),
     username = "bi_team_pt",
     password = bi_team_pt_password,
-    host = "127.0.0.1",
-    port = 3317, 
+    host = "10.29.0.140",
+    port = 3320, 
     dbname = "otomotopl"
   )
 
 dbSqlQuery <-
   dbSendQuery(conDB, sqlQueryOtomotoPL)
 
-dfOtomotoPLAdsIds <- dbFetch(dbSqlQuery)
+dfOtomotoPLAdsIds <- dbFetch(dbSqlQuery, n = -1)
 
 # free the result set
 dbClearResult(dbSqlQuery)
@@ -89,7 +88,7 @@ conDB <-
     RMySQL::MySQL(),
     username = "bi_team_pt",
     password = bi_team_pt_password,
-    host = "127.0.0.1",
+    host = "10.29.0.140",
     port = 3308, 
     dbname = "carspt"
   )
@@ -97,7 +96,7 @@ conDB <-
 dbSqlQuery <-
   dbSendQuery(conDB, sqlQueryStandvirtual)
 
-dfStandvirtualAdsIds <- dbFetch(dbSqlQuery)
+dfStandvirtualAdsIds <- dbFetch(dbSqlQuery, n = -1)
 
 # free the result set
 dbClearResult(dbSqlQuery)
@@ -105,17 +104,37 @@ dbClearResult(dbSqlQuery)
 # disconnect from database  -------------------------------------------------
 dbDisconnect(conDB)
 
-sqlQuery <-
+lstOtomotoPLAdsIds <-
+  paste(dfOtomotoPLAdsIds$id, collapse = ",")
+
+lstStandvirtualAdsIds <-
+  paste(dfStandvirtualAdsIds$id, collapse = ",")
+
+sqlQueryOtomotoPL <-
   "
   SELECT server_path, ad_id, server_date_day, COUNT(*)QtyShowPhoneReplies
   FROM main.hydra_verticals.web
   WHERE trackname = 'reply_phone_show'
-  AND server_path = '/h/v-otomoto-web'
-  AND ad_id IN(6028078457)
+  AND server_path IN ('/h/v-otomoto-web', '/h/v-otomoto-ios', '/h/v-otomoto-android')
+  AND ad_id IN({lstOtomotoPLAdsIds})
   GROUP BY 1, 2, 3
-  LIMIT 10
   ;
   "
+
+sqlQueryOtomotoPL <- glue(sqlQueryOtomotoPL)
+
+sqlQueryStandvirtualPT <-
+  "
+  SELECT server_path, ad_id, server_date_day, COUNT(*)QtyShowPhoneReplies
+  FROM main.hydra_verticals.web
+  WHERE trackname = 'reply_phone_show'
+  AND server_path IN ('/h/v-standvirtual-web', '/h/v-standvirtual-ios', '/h/v-standvirtual-android')
+  AND ad_id IN({lstStandvirtualAdsIds})
+  GROUP BY 1, 2, 3
+  ;
+  "
+
+sqlQueryStandvirtualPT <- glue(sqlQueryStandvirtualPT)
 
 # connect to Yamato ----------------------------------------------------
 
@@ -131,25 +150,111 @@ conDB <-
     password = dwYamatoDbPassword
   )
 
+# OtomotoPL
 
-dbSqlQuery <-
-  RPostgreSQL::dbGetQuery(
-    conDB, sqlQuery
+#web
+resSqlQueryOtomotoPL_web <-
+  dbSendQuery(
+    conDB, sqlQueryOtomotoPL
   )
+
+dfSqlQueryOtomotoPL_web <- fetch(resSqlQueryOtomotoPL_web, n = -1)
+
+# ios
+sqlQueryOtomotoPL_ios <- gsub("main.hydra_verticals.web", "main.hydra_verticals.ios", sqlQueryOtomotoPL)
+
+resSqlQueryOtomotoPL_ios <-
+  dbSendQuery(
+    conDB, sqlQueryOtomotoPL_ios
+  )
+
+dfSqlQueryOtomotoPL_ios <- fetch(resSqlQueryOtomotoPL_ios, n = -1)
+
+# android
+sqlQueryOtomotoPL_and <- gsub("main.hydra_verticals.web", "main.hydra_verticals.android", sqlQueryOtomotoPL)
+
+resSqlQueryOtomotoPL_and <-
+  dbSendQuery(
+    conDB, sqlQueryOtomotoPL_and
+  )
+
+dfSqlQueryOtomotoPL_and <- fetch(resSqlQueryOtomotoPL_and, n = -1)
+
+dfSqlQueryOtomotoPL <- rbind(dfSqlQueryOtomotoPL_web, dfSqlQueryOtomotoPL_ios, dfSqlQueryOtomotoPL_and)
+
+
+# StandvirtualPT
+
+#web
+resSqlQueryStandvirtualPT_web <-
+  dbSendQuery(
+    conDB, sqlQueryStandvirtualPT
+  )
+
+dfSqlQueryStandvirtualPT_web <- fetch(resSqlQueryStandvirtualPT_web, n = -1)
+
+# ios
+sqlQueryStandvirtualPT_ios <- gsub("main.hydra_verticals.web", "main.hydra_verticals.ios", sqlQueryStandvirtualPT)
+
+resSqlQueryStandvirtualPT_ios <-
+  dbSendQuery(
+    conDB, sqlQueryStandvirtualPT_ios
+  )
+
+dfSqlQueryStandvirtualPT_ios <- fetch(resSqlQueryStandvirtualPT_ios, n = -1)
+
+# android
+sqlQueryStandvirtualPT_and <- gsub("main.hydra_verticals.web", "main.hydra_verticals.android", sqlQueryStandvirtualPT)
+
+resSqlQueryStandvirtualPT_and <-
+  dbSendQuery(
+    conDB, sqlQueryStandvirtualPT_and
+  )
+
+dfSqlQueryStandvirtualPT_and <- fetch(resSqlQueryStandvirtualPT_and, n = -1)
+
+dfSqlQueryStandvirtualPT <- rbind(dfSqlQueryStandvirtualPT_web, dfSqlQueryStandvirtualPT_ios, dfSqlQueryStandvirtualPT_and)
 
 dbDisconnect(conDB)
 
 
-# 
-# 
-# 
-# /h/v-otomoto-web;
-# /h/v-otomoto-ios
-# /h/v-otomoto-android;
-# 
-# /h/v-standvirtual-ios
-# /h/v-standvirtual-android
-# 
-# 
-# SELECT DISTINCT server_path FROM main.hydra_verticals.web;
-# 
+# OTOMOTO
+dfShowPhoneByUserByDayOtomotoPL <-
+  dfSqlQueryOtomotoPL %>%
+  group_by(ad_id) %>%
+  summarise(qtyshowphonereplies=sum(qtyshowphonereplies)) %>%
+  inner_join(dfOtomotoPLAdsIds, by = c("ad_id" = "id")) %>%
+  group_by(user_id) %>%
+  summarise(qtyshowphonereplies=sum(qtyshowphonereplies)) %>%
+  select(user_id, qtyshowphonereplies)
+  
+dfCallsByUserByDayOtomotoPL <-
+  dfRaw %>%
+  mutate(call_start_date = as.Date(call_start)) %>%
+  filter(website_name == 'otomotopl', call_start_date >= as.Date('2018-05-19')) %>%
+  group_by(external_user_id) %>%
+  summarise(qtyCalls = sum(n())) %>%
+  right_join(dfShowPhoneByUserByDayOtomotoPL, by = c("external_user_id" = "user_id")) %>%
+  mutate(avgShowPhonesPerCall = qtyshowphonereplies / qtyCalls)
+
+dfCallsByUserByDayOtomotoPL[is.na(dfCallsByUserByDayOtomotoPL)] <- 0
+
+ggplot(dfCallsByUserByDayOtomotoPL)+
+  geom_point(aes(qtyCalls, qtyshowphonereplies))+
+  geom_smooth(aes(qtyCalls, qtyshowphonereplies))+
+  ggtitle("OTOMOTO - Qty Show Phone Vs Qty Tracked Calls")+
+  theme_gdocs()
+
+res <- cor(dfCallsByUserByDayOtomotoPL[, c("qtyCalls", "qtyshowphonereplies")])
+
+corrplot(res, type = "upper",
+         tl.col = "black", tl.srt = 0, addCoef.col = TRUE)
+
+ggplot(dfCallsByUserByDayOtomotoPL)+
+  geom_density(aes(x=avgShowPhonesPerCall), binwidth = 1)+
+  geom_vline(aes(xintercept=median(avgShowPhonesPerCall)),
+                color="blue", linetype="dashed", size=1)+
+  scale_x_continuous(limits = c(0,15), breaks = seq(0,15,1))+
+  scale_y_continuous(labels = scales::percent, limits = c(0,0.16), breaks = seq(0,0.16,0.025))+
+  ggtitle("OTOMOTO - Avg Show Phone Actions by Tracked Call")+
+  theme_gdocs()
