bdc029f8fadc1e3fcc20bfe1493d7a17e19dfbeb
Jose.Baltasar <Jose.Baltasar@olx.com>
2018-06-29 14:14:58 +0100

    Fix to country argument

--- Python ETL/crmintegration/4_analytical_to_base/run_analytical_to_base.py
+++ Python ETL/crmintegration/4_analytical_to_base/run_analytical_to_base.py
@@ -33,7 +33,7 @@ scai.integrationStart(db_conf_file, COD_INTEGRATION, COD_COUNTRY) 	# SCAI
 # Calculate KPIs with data from the Analytical Model
 for i in range(0, len(conf_files)):
 	print('Calculating KPIs in file ' + kpi_files[i] + ' using configuration file ' + conf_files[i] + '...')
-	calculate_kpis.main(db_conf_file, kpi_files[i])
+	calculate_kpis.main(db_conf_file, kpi_files[i], COD_COUNTRY)
 	#input('Ready for next set of KPIs?')
 
 # Send all KPIs to Base
