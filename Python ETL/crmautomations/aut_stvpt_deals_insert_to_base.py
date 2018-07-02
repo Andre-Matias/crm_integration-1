3eb75385ae03914428a76b59d4066b5cd24d7fe8
Jose.Baltasar <Jose.Baltasar@olx.com>
2018-07-02 14:25:32 +0100

    Test path

--- Python ETL/crmautomations/aut_stvpt_deals_insert_to_base.py
+++ Python ETL/crmautomations/aut_stvpt_deals_insert_to_base.py
@@ -7,7 +7,7 @@ import time
 from retry import retry
 import logging
 import threading
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'crmintegration\\0_common'))  # Change this later to a package import
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'crmintegration', '0_common'))  # Change this later to a package import
 import scai
 
 print('Starting Process... ' + time.strftime("%H:%M:%S"))
