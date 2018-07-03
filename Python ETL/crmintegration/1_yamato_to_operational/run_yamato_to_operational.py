9741ed659796303c275772d2c97dc2bcb6825523
Jose.Baltasar <Jose.Baltasar@olx.com>
2018-07-03 13:50:48 +0100

    Fix to process exit

--- Python ETL/crmintegration/1_yamato_to_operational/run_yamato_to_operational.py
+++ Python ETL/crmintegration/1_yamato_to_operational/run_yamato_to_operational.py
@@ -26,7 +26,8 @@ scai_last_execution_status = scai.getLastExecutionStatus(db_conf_file, COD_INTEG
 
 
 if (country_execution_status != 1 and scai_last_execution_status == 1):
-	sys.exit("The integration executed successfuly on last execution. The problem is further ahead.")
+	print ('The integration executed successfuly on last execution. The problem is further ahead.')
+	sys.exit(0)
 
 if (scai_last_execution_status == 2):
 	sys.exit("The integration is already running...")
